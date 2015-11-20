08e86eee01d4f8a1bea97813a831f0061b1d7972
Antony Cherepanov <antony.cherepanov@gmail.com>
2015-11-20 12:24:56 +0300

    boolean.hpp - fix compiler errors

--- src/types/boolean.hpp
+++ src/types/boolean.hpp
@@ -1,10 +1,3 @@
-/*
- * boolean.h
- *
- *  Created on: Nov 6, 2014
- *      Author: Antony Cherepanov
- */
-
 #ifndef BOOLEAN_H_
 #define BOOLEAN_H_
 
@@ -13,69 +6,69 @@
 
 void BoolValues()
 {
-	std::cout << "BoolValues()" << std::endl;
+    std::cout << "BoolValues()" << std::endl;
 
-	// False == 0, True == 1 (or !0)
-	std::cout << "True as int = " << int(true) << std::endl;
-	std::cout << "False as int = " << int(false) << std::endl;
-	std::cout << "0 as bool = " << std::boolalpha <<  bool(0) << std::endl;
-	std::cout << "1 as bool = " << std::boolalpha << bool(1) << std::endl;
-	std::cout << "123456 as bool = " << std::boolalpha << bool(123456) <<
-			std::endl;
-	std::cout << std::endl;
+    // False == 0, True == 1 (or !0)
+    std::cout << "True as int = " << int(true) << std::endl;
+    std::cout << "False as int = " << int(false) << std::endl;
+    std::cout << "0 as bool = " << std::boolalpha << bool(0) << std::endl;
+    std::cout << "1 as bool = " << std::boolalpha << bool(1) << std::endl;
+    std::cout << "123456 as bool = " << std::boolalpha << bool(123456) <<
+            std::endl;
+    std::cout << std::endl;
 }
 
 void BoolArithmetics()
 {
-	std::cout << "BoolArithmetics()" << std::endl;
+    std::cout << "BoolArithmetics()" << std::endl;
 
-	std::cout.setf(std::ios::boolalpha);
-	std::cout << "true + true = " << bool(true + true) << std::endl;
-	std::cout << "true - true = " << bool(true - true) << std::endl;
-	std::cout << "true + false = " << bool(true + false) << std::endl;
-	std::cout << "true - false = " << bool(true - false) << std::endl;
-	std::cout << "false + false = " << bool(false + false) << std::endl;
-	std::cout << "false - false = " << bool(false - false) << std::endl;
-	std::cout << std::endl;
+    std::cout.setf(std::ios::boolalpha);
+    std::cout << "true + true = " << bool(true + true) << std::endl;
+    std::cout << "true - true = " << bool(true - true) << std::endl;
+    std::cout << "true + false = " << bool(true + false) << std::endl;
+    std::cout << "true - false = " << bool(true - false) << std::endl;
+    std::cout << "false + false = " << bool(false + false) << std::endl;
+    std::cout << "false - false = " << bool(false - false) << std::endl;
+    std::cout << std::endl;
 }
 
 void BoolLogicOperations()
 {
-	std::cout << "BoolLogicOperations()" << std::endl;
+    std::cout << "BoolLogicOperations()" << std::endl;
 
-	std::cout.setf(std::ios::boolalpha);
-	std::cout << "AND:" << std::endl;
-	std::cout << "true && true = " << bool(true && true) << std::endl;
-	std::cout << "true && false = " << bool(true && false) << std::endl;
-	std::cout << "false && true = " << bool(false && true) << std::endl;
-	std::cout << "false && false = " << bool(false && false) << std::endl;
-	std::cout << std::endl;
+    std::cout << "AND:" << std::endl;
+    std::cout.setf(std::ios::boolalpha);
+    std::cout << "true && true = " << (true && true) << std::endl;
+    std::cout << "true && false = " << (true && false) << std::endl;
+    std::cout << "false && true = " << (false && true) << std::endl;
+    std::cout << "false && false = " << (false && false) << std::endl;
+    std::cout << std::endl;
 
-	std::cout << "OR:" << std::endl;
-	std::cout << "true || true = " << bool(true || true) << std::endl;
-	std::cout << "true || false = " << bool(true || false) << std::endl;
-	std::cout << "false || true = " << bool(false || true) << std::endl;
-	std::cout << "false || false = " << bool(false || false) << std::endl;
-	std::cout << std::endl;
+    std::cout << "OR:" << std::endl;
+    std::cout << "true || true = " << (true || true) << std::endl;
+    std::cout << "true || false = " << (true || false) << std::endl;
+    std::cout << "false || true = " << (false || true) << std::endl;
+    std::cout << "false || false = " << (false || false) << std::endl;
+    std::cout << std::endl;
 
-	std::cout << "NOT:" << std::endl;
-	std::cout << "!true = " << bool(!true) << std::endl;
-	std::cout << "!false = " << bool(!false) << std::endl;
-	std::cout << std::endl;
+    std::cout << "NOT:" << std::endl;
+    std::cout << "!true = " << (!true) << std::endl;
+    std::cout << "!false = " << (!false) << std::endl;
+    std::cout << std::endl;
 
-	std::cout << "XOR:" << std::endl;
-	std::cout << "true ^ true = " << bool(true ^ true) << std::endl;
-	std::cout << "true ^ false = " << bool(true ^ false) << std::endl;
-	std::cout << "false ^ true = " << bool(false ^ true) << std::endl;
-	std::cout << "false ^ false = " << bool(false ^ false) << std::endl;
-	std::cout << std::endl;
+    std::cout << "XOR:" << std::endl;
+    std::cout << "true ^ true = " << bool(true ^ true) << std::endl;
+    std::cout << "true ^ false = " << bool(true ^ false) << std::endl;
+    std::cout << "false ^ true = " << bool(false ^ true) << std::endl;
+    std::cout << "false ^ false = " << bool(false ^ false) << std::endl;
+    std::cout << std::endl;
 }
 
 void Start()
 {
-	BoolValues();
-	BoolArithmetics();
-	BoolLogicOperations();
+    BoolValues();
+    BoolArithmetics();
+    BoolLogicOperations();
 }
 
 #endif /* BOOLEAN_H_ */
