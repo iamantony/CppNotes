d0c44dc29e5b8535ec62d17da7425f764001465f
Antony Cherepanov <antony.cherepanov@gmail.com>
2015-11-20 12:54:49 +0300

    Tabs to spaces

--- src/classes/base_inheritance.hpp
+++ src/classes/base_inheritance.hpp
@@ -6,63 +6,63 @@
 class Base
 {
 public:
-	int public_data;
+    int public_data;
 
 protected:
-	int protected_data;
+    int protected_data;
 
 public:
-	// Construct Base class object and initialize class members
-	Base() : public_data(0), protected_data(1)
-	{
-		std::cout << "In Base constructor: public_data = " << public_data <<
-				", protected_data = " << protected_data << std::endl;
-	};
+    // Construct Base class object and initialize class members
+    Base() : public_data(0), protected_data(1)
+    {
+        std::cout << "In Base constructor: public_data = " << public_data <<
+                ", protected_data = " << protected_data << std::endl;
+    };
 
-	~Base()
-	{
-		std::cout << "In Base destructor" << std::endl;
-	};
+    ~Base()
+    {
+        std::cout << "In Base destructor" << std::endl;
+    };
 };
 
 class Derived : public Base
 {
 public:
-	// We have access to public and protected members of Base class
-	// and we can change them
-	Derived()
-	{
-		public_data = 42;
-		protected_data++;
-		std::cout << "In Derived constructor: public_data = " << public_data <<
-				", protected_data = " << protected_data << std::endl;
-	};
+    // We have access to public and protected members of Base class
+    // and we can change them
+    Derived()
+    {
+        public_data = 42;
+        protected_data++;
+        std::cout << "In Derived constructor: public_data = " << public_data <<
+                ", protected_data = " << protected_data << std::endl;
+    };
 
-	~Derived()
-	{
-		std::cout << "In Derived destructor" << std::endl;
-	};
+    ~Derived()
+    {
+        std::cout << "In Derived destructor" << std::endl;
+    };
 };
 
 void Start()
 {
-	std::cout << "base_inheritance" << std::endl << std::endl;
+    std::cout << "base_inheritance" << std::endl << std::endl;
 
-	std::cout << "Create Base class object:" << std::endl;
-	Base *baseObject = new Base();
+    std::cout << "Create Base class object:" << std::endl;
+    Base *baseObject = new Base();
 
-	std::cout << "Create Derived class object:" << std::endl;
-	// First of all will be created constructor of parent class and then
-	// constructor of Derived class will be called.
-	Derived *derivedObject = new Derived();
+    std::cout << "Create Derived class object:" << std::endl;
+    // First of all will be created constructor of parent class and then
+    // constructor of Derived class will be called.
+    Derived *derivedObject = new Derived();
 
-	std::cout << "Delete Base class object:" << std::endl;
-	delete baseObject;
+    std::cout << "Delete Base class object:" << std::endl;
+    delete baseObject;
 
-	std::cout << "Delete Derived class object:" << std::endl;
-	// Call child (Derived destructor), in the end call parent class
-	// destructor (Base destructor)
-	delete derivedObject;
+    std::cout << "Delete Derived class object:" << std::endl;
+    // Call child (Derived destructor), in the end call parent class
+    // destructor (Base destructor)
+    delete derivedObject;
 }
 
 #endif /* BASE_H_ */
--- src/classes/multy_inheritance.hpp
+++ src/classes/multy_inheritance.hpp
@@ -4,54 +4,54 @@
 class First
 {
 public:
-	First()
-	{
-		std::cout << "In First constructor" <<  std::endl;
-	};
+    First()
+    {
+        std::cout << "In First constructor" <<  std::endl;
+    };
 
-	~First()
-	{
-		std::cout << "In First destructor" << std::endl;
-	};
+    ~First()
+    {
+        std::cout << "In First destructor" << std::endl;
+    };
 };
 
 class Second
 {
 public:
-	Second()
-	{
-		std::cout << "In Second constructor" <<  std::endl;
-	};
+    Second()
+    {
+        std::cout << "In Second constructor" <<  std::endl;
+    };
 
-	~Second()
-	{
-		std::cout << "In Second destructor" << std::endl;
-	};
+    ~Second()
+    {
+        std::cout << "In Second destructor" << std::endl;
+    };
 };
 
 class Derived : public First, public Second
 {
 public:
-	Derived()
-	{
-		std::cout << "In Derived constructor" <<  std::endl;
-	};
+    Derived()
+    {
+        std::cout << "In Derived constructor" <<  std::endl;
+    };
 
-	~Derived()
-	{
-		std::cout << "In Derived destructor" << std::endl;
-	};
+    ~Derived()
+    {
+        std::cout << "In Derived destructor" << std::endl;
+    };
 };
 
 void Start()
 {
-	std::cout << "multi_inheritance" << std::endl << std::endl;
+    std::cout << "multi_inheritance" << std::endl << std::endl;
 
-	std::cout << "Create Derived class object:" << std::endl;
-	Derived *derivedObject = new Derived();
+    std::cout << "Create Derived class object:" << std::endl;
+    Derived *derivedObject = new Derived();
 
-	std::cout << "Delete Derived class object:" << std::endl;
-	delete derivedObject;
+    std::cout << "Delete Derived class object:" << std::endl;
+    delete derivedObject;
 }
 
 #endif /* MULTY_INHERITANCE_H_ */
