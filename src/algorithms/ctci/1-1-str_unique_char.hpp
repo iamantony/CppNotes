24de7f1ac9de2d31a52c78091663e751237328d8
Antony Cherepanov <antony.cherepanov@gmail.com>
2016-01-11 13:07:07 +0300

    Add solution for task 1.1 in CTIC using string and set

--- src/algorithms/ctci/1-1-str_unique_char.hpp
+++ src/algorithms/ctci/1-1-str_unique_char.hpp
@@ -1,8 +1,34 @@
 #ifndef STR_UNIQUE_CHAR_HPP_
 #define STR_UNIQUE_CHAR_HPP_
 
+#include <iostream>
+#include <string>
+#include <set>
 
+// Implement an algorithm to determine if a string has all unique characters.
+// What if you cannot use additional data structures?
 
+bool IsStrHasAllUniqueChars(const std::string& inputStr)
+{
+    if (inputStr.empty())
+    {
+        return (false);
+    }
 
+    std::set<char> characters;
+    for (int i = 0; i < inputStr.size(); ++i)
+    {
+        if (0 == characters.count(inputStr.at(i)))
+        {
+            characters.insert(inputStr.at(i));
+        }
+        else
+        {
+            return (false);
+        }
+    }
+
+    return (true);
+}
 
 #endif /* STR_UNIQUE_CHAR_HPP_ */
