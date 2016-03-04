147c5201fbc14fa48f5ee314fcc8269beeedbbc8
Antony Cherepanov <antony.cherepanov@gmail.com>
2016-03-04 17:51:28 +0300

    Add templates section. Namespace lookup

---
+++ src/templates/namespacelookup.hpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+struct c1
+{
+    template<typename T>
+    void foo(T value)
+    {
+        std::cout << "In foo<T>(T): " << value << std::endl;
+    }
+};
+
+struct c2 : c1
+{
+    using c1::foo;
+    void foo(double value)
+    {
+        std::cout << "In foo(double): " << value << std::endl;
+    }
+};
+
+void StartNL()
+{
+    c2 c;
+    c.foo(11.2);
+    c.foo(11);
+}
