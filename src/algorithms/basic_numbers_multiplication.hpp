0aa5540033a1a97ff3c224aced0f19a2e9d6b396
Antony Cherepanov <antony.cherepanov@gmail.com>
2016-10-14 14:08:14 +0300

    Modification of basic multiplication algorithm - input arguments are numbers

--- src/algorithms/basic_numbers_multiplication.hpp
+++ src/algorithms/basic_numbers_multiplication.hpp
@@ -1,38 +1,53 @@
 #ifndef BASIC_NUMBERS_MULTIPLICATION_HPP_
 #define BASIC_NUMBERS_MULTIPLICATION_HPP_
 
-#include <string>
+long long int GetNumOfDigits(const long long int& number);
+long long int GetDigitAtPosition(const long long int& number,
+        const long long int& position);
 
 // Standard algorithm of multiplication of two numbers.
-// @input: two numbers, represented as strings
+// @input: two numbers
 // @output: multiplication result
-long long int BasicNumbersMultiplication(const std::string& firstNum,
-        const std::string& secondNum)
+long long int BasicNumbersMultiplication(const long long int& firstNum,
+        const long long int& secondNum)
 {
-    if (firstNum.empty() || secondNum.empty())
+    if (0 == firstNum || 0 == secondNum)
     {
         return 0;
     }
 
-    long long int result = 0;
-    int i = static_cast<int>(secondNum.size() - 1);
-    long long int positionFactorSecond = 1;
-    for (; i >= 0; --i, positionFactorSecond *= 10)
+    // Result will be negative number
+    long long int sign = -1;
+    if ( (firstNum > 0 && secondNum > 0) || (firstNum < 0 && secondNum < 0) )
     {
-        std::string factorStr(1, secondNum.at(static_cast<size_t>(i)));
-        int factor = std::stoi(factorStr);
+        // Result will be positive number
+        sign = 1;
+    }
+
+    // Get positive values of our numbers. We will use them in further
+    // computations
+    const long long int first = std::abs(firstNum);
+    const long long int second = std::abs(secondNum);
+
+    long long int result = 0;
+
+    // Multiply each digit of second number with each digit of the first number
+    const long long int secondNumDigits = GetNumOfDigits(second);
+    for (long long int i = 1, positionFactorSecond = 1; i <= secondNumDigits;
+            ++i, positionFactorSecond *= 10)
+    {
+        long long int factor = GetDigitAtPosition(second, i);
         if (0 == factor)
         {
             continue;
         }
 
         long long int intermediate_result = 0;
-        int j = static_cast<int>(firstNum.size() - 1);
-        long long int positionFactorFirst = 1;
-        for (; j >= 0; --j, positionFactorFirst *= 10)
+        const long long int firstNumDigits = GetNumOfDigits(first);
+        for (long long int j = 1, positionFactorFirst = 1; j <= firstNumDigits;
+                ++j, positionFactorFirst *= 10)
         {
-            std::string multiplicantStr(1, firstNum.at(static_cast<size_t>(j)));
-            int multiplicant = std::stoi(multiplicantStr);
+            long long int multiplicant = GetDigitAtPosition(first, j);
             if (0 == multiplicant)
             {
                 continue;
@@ -45,7 +60,43 @@ long long int BasicNumbersMultiplication(const std::string& firstNum,
         result += intermediate_result;
     }
 
-    return result;
+    return result * sign;
+}
+
+// Get number of digits in number
+// @input: number
+// @output: number of digits
+long long int GetNumOfDigits(const long long int& number)
+{
+    long long int digitsNum = 0;
+    long long int value = std::abs(number);
+    while(0 < value)
+    {
+        ++digitsNum;
+        value /= 10;
+    }
+
+    return digitsNum;
+}
+
+// Get digit of the number at specified position
+// Number:           8 6 3 7
+// Digits positions: 4 3 2 1
+long long int GetDigitAtPosition(const long long int& number,
+        const long long int& position)
+{
+    if (position <= 0)
+    {
+        return 0;
+    }
+
+    long long int value = number;
+    for (long long int i = 1; i < position; ++i)
+    {
+        value /= 10;
+    }
+
+    return value % 10;
 }
 
 #endif /* BASIC_NUMBERS_MULTIPLICATION_HPP_ */
--- test/algorithms/test_basic_numbers_multiplication.hpp
+++ test/algorithms/test_basic_numbers_multiplication.hpp
@@ -8,48 +8,34 @@
 
 #include "algorithms/basic_numbers_multiplication.hpp"
 
-BOOST_AUTO_TEST_CASE(test_empty_numbers)
+BOOST_AUTO_TEST_CASE(test_basic_null_numbers)
 {
-    BOOST_CHECK(0 == BasicNumbersMultiplication(std::string(), std::string()));
-    BOOST_CHECK(0 == BasicNumbersMultiplication(std::string("123"),
-            std::string()));
-
-    BOOST_CHECK(0 == BasicNumbersMultiplication(std::string(),
-            std::string("456")));
+    BOOST_CHECK(0 == BasicNumbersMultiplication(0, 0));
+    BOOST_CHECK(0 == BasicNumbersMultiplication(123, 0));
+    BOOST_CHECK(0 == BasicNumbersMultiplication(0, 456));
 }
 
-BOOST_AUTO_TEST_CASE(test_null_numbers)
+BOOST_AUTO_TEST_CASE(test_basic_small_numbers)
 {
-    BOOST_CHECK(0 == BasicNumbersMultiplication(std::string("0"),
-            std::string("0")));
-
-    BOOST_CHECK(0 == BasicNumbersMultiplication(std::string("123"),
-                std::string("0")));
-
-    BOOST_CHECK(0 == BasicNumbersMultiplication(std::string("0"),
-                std::string("123")));
+    BOOST_CHECK(1 == BasicNumbersMultiplication(1, 1));
+    BOOST_CHECK(20 == BasicNumbersMultiplication(10, 2));
+    BOOST_CHECK(770 == BasicNumbersMultiplication(14, 55));
 }
 
-BOOST_AUTO_TEST_CASE(test_small_numbers)
+BOOST_AUTO_TEST_CASE(test_basic_big_numbers)
 {
-    BOOST_CHECK(1 == BasicNumbersMultiplication(std::string("1"),
-            std::string("1")));
-
-    BOOST_CHECK(20 == BasicNumbersMultiplication(std::string("10"),
-                std::string("2")));
-
-    BOOST_CHECK(770 == BasicNumbersMultiplication(std::string("14"),
-                std::string("55")));
-}
-
-BOOST_AUTO_TEST_CASE(test_big_numbers)
-{
-    BOOST_CHECK(3085153753124 == BasicNumbersMultiplication(std::string("895372"),
-            std::string("3445667")));
-
+    BOOST_CHECK(3085153753124 == BasicNumbersMultiplication(895372, 3445667));
     BOOST_CHECK(121932631112635269 ==
-            BasicNumbersMultiplication(std::string("987654321"),
-                    std::string("123456789")));
+            BasicNumbersMultiplication(987654321, 123456789));
+}
+
+BOOST_AUTO_TEST_CASE(test_basic_numbers_with_sign)
+{
+    BOOST_CHECK(3085153753124 == BasicNumbersMultiplication(-895372, -3445667));
+    BOOST_CHECK(-121932631112635269 ==
+            BasicNumbersMultiplication(-987654321, 123456789));
+    BOOST_CHECK(-121932631112635269 ==
+                    BasicNumbersMultiplication(987654321, -123456789));
 }
 
 #endif /* TEST_BASIC_NUMBERS_MULTIPLICATION_HPP_ */
