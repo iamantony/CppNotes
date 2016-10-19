cd67eb98b029de5efb474257ff1c3014acfcb1fc
Antony Cherepanov <antony.cherepanov@gmail.com>
2016-10-19 13:24:14 +0300

    Add TODOs

--- src/algorithms/strassen_matrix_multiplication.hpp
+++ src/algorithms/strassen_matrix_multiplication.hpp
@@ -3,9 +3,15 @@
 
 #include "types/matrix.hpp"
 
+// TODO:
+// - add SubMatrix class for representing sub-matrices (store pointer to
+// matrix, size of the sub-matrix and start point). Implement addition,
+// subtraction, raw copy to separate matrix object.
+// - implement Strassen algorithm
+
 // Strassen algorithm for matrix multiplication
 
-// @input: two square matrices with equal sizes.
+// @input: two square matrices. Size of the matrix should be power of 2.
 // @output: result matrix
 template<typename T>
 Matrix<T> StrassenMatrixMultiplication(const Matrix<T>& left,
