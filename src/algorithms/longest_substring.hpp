07266e3cfb6346d621969d886f8838b6c18f692f
iamantony <antony.cherepanov@gmail.com>
2017-10-29 21:01:34 +0300

    Minor changes

--- src/algorithms/longest_substring.hpp
+++ src/algorithms/longest_substring.hpp
@@ -8,8 +8,8 @@
 
 // Examples:
 // Given "abcabcbb", the answer is "abc", which the length is 3.
-//Given "bbbbb", the answer is "b", with the length of 1.
-//Given "pwwkew", the answer is "wke", with the length of 3. Note that the
+// Given "bbbbb", the answer is "b", with the length of 1.
+// Given "pwwkew", the answer is "wke", with the length of 3. Note that the
 // answer must be a substring, "pwke" is a subsequence and not a substring.
 
 #include <string>
