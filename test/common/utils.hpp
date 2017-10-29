e5c6c15bdd5b4706906838ecf55391253257acad
iamantony <antony.cherepanov@gmail.com>
2017-10-29 21:02:10 +0300

    Fix: add code to utils.hpp

--- test/common/utils.hpp
+++ test/common/utils.hpp
@@ -1,5 +1,13 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
+#include <limits>
+
+template<typename T>
+bool areEqual(const T& left, const T& right)
+{
+    return std::fabs(left - right) <= std::numeric_limits<T>::epsilon();
+}
+
 #endif // UTILS_HPP
 
