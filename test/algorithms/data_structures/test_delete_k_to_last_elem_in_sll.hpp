0f990656529a3f51d7b6b2876c2e150f5807655e
iamantony <antony.cherepanov@gmail.com>
2017-12-11 11:01:09 +0300

    Add algorithm 'Delete k-to-last element in SLL'

---
+++ test/algorithms/data_structures/test_delete_k_to_last_elem_in_sll.hpp
@@ -0,0 +1,53 @@
+#ifndef TEST_DELETE_K_TO_LAST_ELEM_IN_SLL_HPP
+#define TEST_DELETE_K_TO_LAST_ELEM_IN_SLL_HPP
+
+#include <boost/test/unit_test.hpp>
+
+#include "algorithms/data_structures/delete_k_to_last_elem_in_sll.hpp"
+
+BOOST_AUTO_TEST_CASE(test_dktlesll_one_elem) {
+    NodeSLL<int>* sll = new NodeSLL<int>(10);
+
+    DeleteKToLastElem::Solution solution;
+    NodeSLL<int>* result = solution.deleteKthFromEnd(sll, 1);
+
+    BOOST_CHECK(nullptr == result);
+}
+
+BOOST_AUTO_TEST_CASE(test_dktlesll_delete_last)
+{
+    NodeSLL<int>* sll = CreateSLL<int>({1, 2, 3, 4});
+
+    DeleteKToLastElem::Solution solution;
+    NodeSLL<int>* result = solution.deleteKthFromEnd(sll, 1);
+
+    std::vector<int> expectedValues = {1, 2, 3};
+    BOOST_CHECK(expectedValues == ValuesInSLL(&result));
+    DeleteSLL(&result);
+}
+
+BOOST_AUTO_TEST_CASE(test_dktlesll_delete_one_before_last)
+{
+    NodeSLL<int>* sll = CreateSLL<int>({1, 2, 3, 4});
+
+    DeleteKToLastElem::Solution solution;
+    NodeSLL<int>* result = solution.deleteKthFromEnd(sll, 2);
+
+    std::vector<int> expectedValues = {1, 2, 4};
+    BOOST_CHECK(expectedValues == ValuesInSLL(&result));
+    DeleteSLL(&result);
+}
+
+BOOST_AUTO_TEST_CASE(test_dktlesll_delete_three_before_last)
+{
+    NodeSLL<int>* sll = CreateSLL<int>({1, 2, 3, 4});
+
+    DeleteKToLastElem::Solution solution;
+    NodeSLL<int>* result = solution.deleteKthFromEnd(sll, 3);
+
+    std::vector<int> expectedValues = {1, 3, 4};
+    BOOST_CHECK(expectedValues == ValuesInSLL(&result));
+    DeleteSLL(&result);
+}
+
+#endif // TEST_DELETE_K_TO_LAST_ELEM_IN_SLL_HPP
