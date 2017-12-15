2277e1e5ee42cfa93b202b6b8929da702a5063ad
iamantony <antony.cherepanov@gmail.com>
2017-12-15 15:49:42 +0300

    Add equalDoubles() to equality.hpp

--- src/common/equality.hpp
+++ src/common/equality.hpp
@@ -4,15 +4,18 @@
 #include <limits>
 
 template<typename T>
-bool equal(const T& left, const T& right)
-{
+bool equal(const T& left, const T& right) {
     return left == right;
 }
 
 template<>
-bool equal<double>(const double& left, const double& right)
-{
+bool equal<double>(const double& left, const double& right) {
     return std::fabs(left - right) <= std::numeric_limits<double>::epsilon();
+}
+
+bool equalDoubles(const double& left, const double& right,
+                  const double& epsilon) {
+    return std::fabs(left - right) <= epsilon;
 }
 
 #endif // UTILS_HPP
