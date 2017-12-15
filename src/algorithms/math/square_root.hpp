914d035c32b883c88052f3e4d6427cbda64d7dfa
iamantony <antony.cherepanov@gmail.com>
2017-12-15 15:50:45 +0300

    Add Babylonial square root calc algorithm

--- src/algorithms/math/square_root.hpp
+++ src/algorithms/math/square_root.hpp
@@ -9,26 +9,41 @@ namespace SquareRoot {
 // where S is a value for which square root we want to find
 
 // So: sqrt(S) = sqrt(a) * (2^n) = 2^n
-int RoughSR(const int& value) {
+int RoughSR(int value) {
     if (value <= 0) {
         return -1;
     }
-    else if (value == 1) {
-        return 0;
-    }
-    else if (value == 2) {
+    else if (value == 1 || value == 2) {
         return 1;
     }
 
     int lastOneBitPos = 0;
-    int temp = value;
-    while(temp > 0) {
-        temp >>= 1;
+    while(value > 0) {
+        value >>= 1;
         ++lastOneBitPos;
     }
 
     int power = lastOneBitPos / 2 ;
     int root = 1 << power;
+    return root;
+}
+
+// https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method
+double BabylonialSR(int value, const double& error = 0.0001) {
+    if (value <= 0 || error <= 0.0) {
+        return 0.0;
+    }
+
+    double root = RoughSR(value);
+    while(true) {
+        double newRoot = (root + value/root) / 2.0;
+        double diff = (root > newRoot) ? root - newRoot : newRoot - root;
+        root = newRoot;
+        if (diff <= error) {
+            break;
+        }
+    }
+
     return root;
 }
 
