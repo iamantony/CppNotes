3bb797c7e2eca42fe203ebe5a6f192dfea4b3b2b
iamantony <antony.cherepanov@gmail.com>
2018-02-22 12:07:19 +0300

    Add algorithm: move zeroes to the end of the array

--- test/test.cpp
+++ test/test.cpp
@@ -11,6 +11,7 @@
 #include "algorithms/data_structures/test_rotate_array.hpp"
 #include "algorithms/data_structures/test_duplicates_in_array.hpp"
 #include "algorithms/data_structures/test_intersection_of_two_arrays.hpp"
+#include "algorithms/data_structures/test_move_zeroes_to_the_end_of_array.hpp"
 
 #include "algorithms/math/test_add_two_numbers_as_linked_list.hpp"
 #include "algorithms/math/test_basic_matrix_multiplication.hpp"
