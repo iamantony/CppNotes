de7f634bc01108ae123476740e84631215de4e7e
iamantony <antony.cherepanov@gmail.com>
2018-03-06 13:02:59 +0300

    Update tests for LinkedList

--- test/types/ds/test_linked_list.hpp
+++ test/types/ds/test_linked_list.hpp
@@ -5,10 +5,11 @@
 
 #include "types/ds/linked_list.hpp"
 
-BOOST_AUTO_TEST_CASE(test_ll_create_linkedlist)
+BOOST_AUTO_TEST_CASE(test_ll_create_empty_linkedlist)
 {
     DS::LinkedList<int> list;
-    BOOST_CHECK(true);
+    BOOST_CHECK(list.isEmpty());
+    BOOST_CHECK(0 == list.size());
 }
 
 BOOST_AUTO_TEST_CASE(test_ll_create_with_size)
@@ -23,20 +24,78 @@ BOOST_AUTO_TEST_CASE(test_ll_create_with_size)
     BOOST_CHECK(42 == list.at(4));
 }
 
+BOOST_AUTO_TEST_CASE(test_ll_create_from_vector)
+{
+    std::vector<int> vector({1, 2, 3});
+    DS::LinkedList<int> list(vector);
+    BOOST_CHECK(false == list.isEmpty());
+    BOOST_CHECK(3 == list.size());
+    BOOST_CHECK(1 == list.at(0));
+    BOOST_CHECK(2 == list.at(1));
+    BOOST_CHECK(3 == list.at(2));
+}
+
 BOOST_AUTO_TEST_CASE(test_ll_add_elements)
 {
     DS::LinkedList<int> list;
     BOOST_CHECK(list.isEmpty());
 
-    list.addLast(1);
-    list.addLast(2);
-    list.addLast(3);
-    list.addLast(4);
+    list.pushFront(1);
+    list.pushFront(2);
+    list.pushBack(3);
+    list.pushBack(4);
+    list.insert(0, 10);
+    list.insert(3, 11);
+    list.insert(6, 12);
+    list.insert(42, 13);
 
     BOOST_CHECK(false == list.isEmpty());
-    BOOST_CHECK(4 == list.size());
-    BOOST_CHECK(1 == list.front());
-    BOOST_CHECK(4 == list.back());
+    BOOST_CHECK(8 == list.size());
+
+    BOOST_CHECK(10 == list.at(0));
+    BOOST_CHECK(2 == list.at(1));
+    BOOST_CHECK(1 == list.at(2));
+    BOOST_CHECK(11 == list.at(3));
+    BOOST_CHECK(3 == list.at(4));
+    BOOST_CHECK(4 == list.at(5));
+    BOOST_CHECK(12 == list.at(6));
+    BOOST_CHECK(13 == list.at(7));
+}
+
+BOOST_AUTO_TEST_CASE(test_ll_access_elements)
+{
+    {
+        DS::LinkedList<int> list;
+
+        BOOST_CHECK_THROW(list.front(), std::length_error);
+        BOOST_CHECK_THROW(list.back(), std::length_error);
+        BOOST_CHECK_THROW(list.at(3), std::length_error);
+    }
+
+    {
+        std::vector<int> vector({1, 2, 3, 4, 5});
+        DS::LinkedList<int> list(vector);
+
+        BOOST_CHECK(1 == list.front());
+        BOOST_CHECK(5 == list.back());
+        BOOST_CHECK(1 == list.at(0));
+        BOOST_CHECK(3 == list.at(2));
+        BOOST_CHECK(5 == list.at(4));
+
+        BOOST_CHECK_THROW(list.at(5), std::runtime_error);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(test_ll_access_n_from_end)
+{
+    std::vector<int> vector({1, 2, 3, 4, 5});
+    DS::LinkedList<int> list(vector);
+
+    BOOST_CHECK(5 == list.valueNFromEnd(0));
+    BOOST_CHECK(4 == list.valueNFromEnd(1));
+    BOOST_CHECK(1 == list.valueNFromEnd(4));
+
+    BOOST_CHECK_THROW(list.valueNFromEnd(5), std::runtime_error);
 }
 
 BOOST_AUTO_TEST_CASE(test_ll_clear)
@@ -46,168 +105,79 @@ BOOST_AUTO_TEST_CASE(test_ll_clear)
     BOOST_CHECK(true == list.isEmpty());
     BOOST_CHECK(0 == list.size());
 
-    list.addLast(1);
-    list.addLast(2);
+    list.pushBack(1);
+    list.pushBack(2);
+    BOOST_CHECK(false == list.isEmpty());
+    BOOST_CHECK(2 == list.size());
+
     list.clear();
 
     BOOST_CHECK(true == list.isEmpty());
     BOOST_CHECK(0 == list.size());
 }
 
-BOOST_AUTO_TEST_CASE(test_ll_first_last)
+BOOST_AUTO_TEST_CASE(test_ll_remove_elements)
 {
-    DS::LinkedList<unsigned int> list;
+    std::vector<int> vector({1, 2, 3, 4, 5});
+    DS::LinkedList<int> list(vector);
 
-    BOOST_CHECK_THROW(list.front(), std::length_error);
-    BOOST_CHECK_THROW(list.back(), std::length_error);
-
-    list.addLast(186);
-    list.addLast(187);
-    list.addLast(28786);
-
-    BOOST_CHECK(186 == list.front());
-    BOOST_CHECK(28786 == list.back());
-}
-
-BOOST_AUTO_TEST_CASE(test_ll_at)
-{
-    DS::LinkedList<unsigned int> list;
-
-    BOOST_CHECK_THROW(list.at(0), std::length_error);
-
-    list.addLast(186);
-    list.addLast(187);
-    list.addLast(28786);
-
-    BOOST_CHECK_THROW(list.at(-156), std::runtime_error);
-    BOOST_CHECK_THROW(list.at(10), std::runtime_error);
-
-    BOOST_CHECK(186 == list.at(0));
-    BOOST_CHECK(187 == list.at(1));
-    BOOST_CHECK(28786 == list.at(2));
-}
-
-BOOST_AUTO_TEST_CASE(test_ll_add_first)
-{
-    DS::LinkedList<unsigned int> list;
-    list.addFirst(186);
-    list.addLast(187);
-    list.addFirst(28786);
-
-    BOOST_CHECK(3 == list.size());
-    BOOST_CHECK(28786 == list.front());
-    BOOST_CHECK(186 == list.at(1));
-    BOOST_CHECK(187 == list.back());
-}
-
-BOOST_AUTO_TEST_CASE(test_ll_add_last)
-{
-    DS::LinkedList<unsigned int> list;
-    list.addLast(186);
-    list.addLast(187);
-
-    BOOST_CHECK(186 == list.front());
-    BOOST_CHECK(187 == list.back());
-}
-
-BOOST_AUTO_TEST_CASE(test_ll_insert)
-{
-    DS::LinkedList<unsigned int> list;
-    list.insert(-100, 1);
-    list.insert(1500, 2);
-    list.insert(0, 3);
-    list.insert(1, 4);
-
-    BOOST_CHECK(3 == list.at(0));
-    BOOST_CHECK(4 == list.at(1));
-    BOOST_CHECK(1 == list.at(2));
-    BOOST_CHECK(2 == list.at(3));
-}
-
-BOOST_AUTO_TEST_CASE(test_ll_remove)
-{
-    DS::LinkedList<int> list;
-    list.remove(0);
-    list.addLast(-100);
-    list.addLast(1500);
-    list.addLast(0);
-    list.addLast(1);
-
-    list.remove(-100);
-    list.remove(1501);
-
-    list.remove(1);
-    BOOST_CHECK(3 == list.size());
-    BOOST_CHECK(-100 == list.at(0));
-    BOOST_CHECK(0 == list.at(1));
-    BOOST_CHECK(1 == list.at(2));
-
-    list.remove(2);
-    BOOST_CHECK(2 == list.size());
-    BOOST_CHECK(-100 == list.at(0));
-    BOOST_CHECK(0 == list.at(1));
-
-    list.remove(0);
-    BOOST_CHECK(1 == list.size());
-    BOOST_CHECK(0 == list.at(0));
-
-    list.remove(0);
-    BOOST_CHECK(0 == list.size());
-    BOOST_CHECK(true == list.isEmpty());
-}
-
-BOOST_AUTO_TEST_CASE(test_ll_remove_first)
-{
-    DS::LinkedList<unsigned int> list;
-    list.addLast(1);
-    list.addLast(2);
-    list.addLast(3);
-    list.addLast(4);
-
-    list.removeFirst();
+    list.popFront();
+    list.popBack();
     BOOST_CHECK(3 == list.size());
     BOOST_CHECK(2 == list.at(0));
     BOOST_CHECK(3 == list.at(1));
     BOOST_CHECK(4 == list.at(2));
 
-    list.removeFirst();
+    list.deleteItem(1);
     BOOST_CHECK(2 == list.size());
-    BOOST_CHECK(3 == list.at(0));
+    BOOST_CHECK(2 == list.at(0));
     BOOST_CHECK(4 == list.at(1));
 
-    list.removeFirst();
+    list.deleteItem(0);
+    list.deleteItem(10);
     BOOST_CHECK(1 == list.size());
     BOOST_CHECK(4 == list.at(0));
 
-    list.removeFirst();
+    list.deleteItem(0);
     BOOST_CHECK(0 == list.size());
+    BOOST_CHECK(true == list.isEmpty());
 }
 
-BOOST_AUTO_TEST_CASE(test_ll_remove_last)
+BOOST_AUTO_TEST_CASE(test_ll_remove_values)
 {
-    DS::LinkedList<unsigned int> list;
-    list.addLast(1);
-    list.addLast(2);
-    list.addLast(3);
-    list.addLast(4);
+    std::vector<int> vector({1, 1, 3, 4, 3, 6, 3});
+    DS::LinkedList<int> list(vector);
 
-    list.removeLast();
-    BOOST_CHECK(3 == list.size());
+    list.remove(3);
+    BOOST_CHECK(4 == list.size());
     BOOST_CHECK(1 == list.at(0));
-    BOOST_CHECK(2 == list.at(1));
-    BOOST_CHECK(3 == list.at(2));
+    BOOST_CHECK(1 == list.at(1));
+    BOOST_CHECK(4 == list.at(2));
+    BOOST_CHECK(6 == list.at(3));
 
-    list.removeLast();
+    list.remove(1);
     BOOST_CHECK(2 == list.size());
-    BOOST_CHECK(1 == list.at(0));
-    BOOST_CHECK(2 == list.at(1));
+    BOOST_CHECK(4 == list.at(0));
+    BOOST_CHECK(6 == list.at(1));
 
-    list.removeLast();
-    BOOST_CHECK(1 == list.size());
-    BOOST_CHECK(1 == list.at(0));
+    list.remove(15);
+    BOOST_CHECK(2 == list.size());
+}
 
-    list.removeLast();
-    BOOST_CHECK(0 == list.size());
+BOOST_AUTO_TEST_CASE(test_ll_reverse)
+{
+    std::vector<int> vector({1, 2, 3, 4, 3, 6, 3});
+    DS::LinkedList<int> list(vector);
+
+    list.reverse();
+    BOOST_CHECK(7 == list.size());
+    BOOST_CHECK(3 == list.at(0));
+    BOOST_CHECK(6 == list.at(1));
+    BOOST_CHECK(3 == list.at(2));
+    BOOST_CHECK(4 == list.at(3));
+    BOOST_CHECK(3 == list.at(4));
+    BOOST_CHECK(2 == list.at(5));
+    BOOST_CHECK(1 == list.at(6));
 }
 
 #endif /* TEST_LINKED_LIST_H_ */
