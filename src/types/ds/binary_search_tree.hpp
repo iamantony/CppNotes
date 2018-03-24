63ec7effa3f846f7a8f7d2dcb2c955d2dc659f26
iamantony <antony.cherepanov@gmail.com>
2018-03-24 15:26:34 +0300

    Minor style changes in BST class

--- src/types/ds/binary_search_tree.hpp
+++ src/types/ds/binary_search_tree.hpp
@@ -10,7 +10,6 @@ namespace DS {
 template <typename T>
 class BSTree
 {
-    // == DATA ==
 private:
     class Node
     {
@@ -36,7 +35,6 @@ private:
 
     Node* root;
 
-    // == METHODS ==
 public:
     BSTree() : root(nullptr) {}
     ~BSTree() { clear(); }
@@ -81,42 +79,34 @@ private:
 };
 
 template<typename T>
-BSTree<T>::Node::~Node()
-{
-    if (nullptr != m_left)
-    {
+BSTree<T>::Node::~Node() {
+    if (nullptr != m_left) {
         delete m_left;
         m_left = nullptr;
     }
 
-    if (nullptr != m_right)
-    {
+    if (nullptr != m_right) {
         delete m_right;
         m_right = nullptr;
     }
 }
 
 template<typename T>
-void BSTree<T>::clear()
-{
-    if (nullptr != root)
-    {
+void BSTree<T>::clear() {
+    if (nullptr != root) {
         delete root;
         root = nullptr;
     }
 }
 
 template<typename T>
-int BSTree<T>::size() const
-{
+int BSTree<T>::size() const {
     return calcSize(root);
 }
 
 template<typename T>
-int BSTree<T>::calcSize(const Node* node) const
-{
-    if (nullptr == node)
-    {
+int BSTree<T>::calcSize(const Node* node) const {
+    if (nullptr == node) {
         return 0;
     }
 
@@ -124,69 +114,55 @@ int BSTree<T>::calcSize(const Node* node) const
 }
 
 template<typename T>
-bool BSTree<T>::isEmpty() const
-{
+bool BSTree<T>::isEmpty() const {
     return (nullptr == root);
 }
 
 template<typename T>
-void BSTree<T>::insert(const T& value)
-{
-    if (nullptr == root)
-    {
+void BSTree<T>::insert(const T& value) {
+    if (nullptr == root) {
         root = new Node(value);
     }
-    else
-    {
+    else {
         insertValue(root, value);
     }
 }
 
 template<typename T>
-void BSTree<T>::insertValue(Node* node, const T& value)
-{
-    if (nullptr == node)
-    {
+void BSTree<T>::insertValue(Node* node, const T& value) {
+    if (nullptr == node) {
         throw std::runtime_error("Can't insert to null node");
         return;
     }
 
-    if (value < node->getValue())
-    {
-        if (nullptr == node->getLeftNode())
-        {
+    if (value < node->getValue()) {
+        if (nullptr == node->getLeftNode()) {
             Node* leftNode = new Node(value);
             node->setLeftNode(leftNode);
         }
-        else
-        {
+        else {
             insertValue(node->getLeftNode(), value);
         }
     }
-    else
-    {
-        if (nullptr == node->getRightNode())
-        {
+    else {
+        if (nullptr == node->getRightNode()) {
             Node* rightNode = new Node(value);
             node->setRightNode(rightNode);
         }
-        else
-        {
+        else {
             insertValue(node->getRightNode(), value);
         }
     }
 }
 
 template<typename T>
-bool BSTree<T>::contains(const T& value) const
-{
+bool BSTree<T>::contains(const T& value) const {
     return (findNodeWithValue(root, value) != nullptr);
 }
 
 template<typename T>
 typename BSTree<T>::Node* BSTree<T>::findNodeWithValue(
-        Node* node, const T& value) const
-{
+        Node* node, const T& value) const {
     if (nullptr == node) {
         return nullptr;
     }
@@ -202,41 +178,33 @@ typename BSTree<T>::Node* BSTree<T>::findNodeWithValue(
 }
 
 template<typename T>
-void BSTree<T>::deleteValue(const T& value)
-{
+void BSTree<T>::deleteValue(const T& value) {
     root = deleteValueNode(root, value);
 }
 
 template<typename T>
 typename BSTree<T>::Node* BSTree<T>::deleteValueNode(
-        Node* node, const T& value)
-{
-    if (nullptr == node)
-    {
+        Node* node, const T& value) {
+    if (nullptr == node) {
         return node;
     }
 
-    if (value < node->getValue())
-    {
+    if (value < node->getValue()) {
         Node* resultLeft = deleteValueNode(node->getLeftNode(), value);
         node->setLeftNode(resultLeft);
     }
-    else if (node->getValue() < value)
-    {
+    else if (node->getValue() < value) {
         Node* resultRight = deleteValueNode(node->getRightNode(), value);
         node->setRightNode(resultRight);
     }
-    else
-    {
-        if (nullptr == node->getLeftNode())
-        {
+    else {
+        if (nullptr == node->getLeftNode()) {
             Node* temp = node->getRightNode();
             node->setRightNode(nullptr);
             delete node;
             return temp;
         }
-        else if (nullptr == node->getRightNode())
-        {
+        else if (nullptr == node->getRightNode()) {
             Node* temp = node->getLeftNode();
             node->setLeftNode(nullptr);
             delete node;
@@ -258,16 +226,13 @@ typename BSTree<T>::Node* BSTree<T>::deleteValueNode(
 }
 
 template<typename T>
-int BSTree<T>::maxDepth() const
-{
+int BSTree<T>::maxDepth() const {
     return calcMaxDepth(root);
 }
 
 template<typename T>
-int BSTree<T>::calcMaxDepth(const Node* node) const
-{
-    if (nullptr == node)
-    {
+int BSTree<T>::calcMaxDepth(const Node* node) const {
+    if (nullptr == node) {
         return 0;
     }
 
@@ -277,16 +242,13 @@ int BSTree<T>::calcMaxDepth(const Node* node) const
 }
 
 template<typename T>
-T BSTree<T>::minValue() const
-{
-    if (nullptr == root)
-    {
+T BSTree<T>::minValue() const {
+    if (nullptr == root) {
         return T();
     }
 
     Node* node = minValueNode(root);
-    if (nullptr == node)
-    {
+    if (nullptr == node) {
         return T();
     }
 
@@ -294,16 +256,13 @@ T BSTree<T>::minValue() const
 }
 
 template<typename T>
-typename BSTree<T>::Node* BSTree<T>::minValueNode(Node* node) const
-{
-    if (nullptr == node)
-    {
+typename BSTree<T>::Node* BSTree<T>::minValueNode(Node* node) const {
+    if (nullptr == node) {
         return nullptr;
     }
 
     Node* iNode = node;
-    while(iNode->getLeftNode() != nullptr)
-    {
+    while(iNode->getLeftNode() != nullptr) {
         iNode = iNode->getLeftNode();
     }
 
@@ -311,16 +270,13 @@ typename BSTree<T>::Node* BSTree<T>::minValueNode(Node* node) const
 }
 
 template<typename T>
-T BSTree<T>::maxValue() const
-{
-    if (nullptr == root)
-    {
+T BSTree<T>::maxValue() const {
+    if (nullptr == root) {
         return T();
     }
 
     Node* node = maxValueNode(root);
-    if (nullptr == node)
-    {
+    if (nullptr == node) {
         return T();
     }
 
@@ -328,16 +284,13 @@ T BSTree<T>::maxValue() const
 }
 
 template<typename T>
-typename BSTree<T>::Node* BSTree<T>::maxValueNode(Node* node) const
-{
-    if (nullptr == node)
-    {
+typename BSTree<T>::Node* BSTree<T>::maxValueNode(Node* node) const {
+    if (nullptr == node) {
         return nullptr;
     }
 
     Node* iNode = node;
-    while(iNode->getRightNode() != nullptr)
-    {
+    while(iNode->getRightNode() != nullptr) {
         iNode = iNode->getRightNode();
     }
 
@@ -345,8 +298,7 @@ typename BSTree<T>::Node* BSTree<T>::maxValueNode(Node* node) const
 }
 
 template<typename T>
-std::vector<T> BSTree<T>::printTree() const
-{
+std::vector<T> BSTree<T>::printTree() const {
     std::vector<T> values;
     printNode(root, values);
 
@@ -354,29 +306,24 @@ std::vector<T> BSTree<T>::printTree() const
 }
 
 template<typename T>
-void BSTree<T>::printNode(const Node* node, std::vector<T>& values) const
-{
-    if (nullptr == node)
-    {
+void BSTree<T>::printNode(const Node* node, std::vector<T>& values) const {
+    if (nullptr == node) {
         return;
     }
 
-    if (nullptr != node->getLeftNode())
-    {
+    if (nullptr != node->getLeftNode()) {
         printNode(node->getLeftNode(), values);
     }
 
     values.push_back(node->getValue());
 
-    if (nullptr != node->getRightNode())
-    {
+    if (nullptr != node->getRightNode()) {
         printNode(node->getRightNode(), values);
     }
 }
 
 template<typename T>
-std::vector<T> BSTree<T>::printTreePostorder() const
-{
+std::vector<T> BSTree<T>::printTreePostorder() const {
     std::vector<T> values;
     printNodePostorder(root, values);
 
@@ -385,20 +332,16 @@ std::vector<T> BSTree<T>::printTreePostorder() const
 
 template<typename T>
 void BSTree<T>::printNodePostorder(const Node* node,
-        std::vector<T>& values) const
-{
-    if (nullptr == node)
-    {
+        std::vector<T>& values) const {
+    if (nullptr == node) {
         return;
     }
 
-    if (nullptr != node->getLeftNode())
-    {
+    if (nullptr != node->getLeftNode()) {
         printNodePostorder(node->getLeftNode(), values);
     }
 
-    if (nullptr != node->getRightNode())
-    {
+    if (nullptr != node->getRightNode()) {
         printNodePostorder(node->getRightNode(), values);
     }
 
@@ -406,39 +349,36 @@ void BSTree<T>::printNodePostorder(const Node* node,
 }
 
 template<typename T>
-std::vector<std::vector<T>> BSTree<T>::getPaths() const
-{
+std::vector<std::vector<T>> BSTree<T>::getPaths() const {
     std::vector<std::vector<T>> paths = getNodePaths(root);
-    auto func = [](std::vector<T>& vec){std::reverse(vec.begin(), vec.end());};
+    auto func = [](std::vector<T>& vec){
+        std::reverse(vec.begin(), vec.end());
+    };
+
     std::for_each(paths.begin(), paths.end(), func);
 
     return paths;
 }
 
 template<typename T>
-std::vector<std::vector<T>> BSTree<T>::getNodePaths(const Node* node) const
-{
-    if (nullptr == node)
-    {
+std::vector<std::vector<T>> BSTree<T>::getNodePaths(const Node* node) const {
+    if (nullptr == node) {
         return std::vector<std::vector<T>>();
     }
 
     std::vector<std::vector<T>> leftPaths, rightPaths;
-    if (nullptr != node->getLeftNode())
-    {
+    if (nullptr != node->getLeftNode()) {
         leftPaths = getNodePaths(node->getLeftNode());
     }
 
-    if (nullptr != node->getRightNode())
-    {
+    if (nullptr != node->getRightNode()) {
         rightPaths = getNodePaths(node->getRightNode());
     }
 
     std::vector<std::vector<T>> result;
     std::copy(leftPaths.begin(), leftPaths.end(), std::back_inserter(result));
     std::copy(rightPaths.begin(), rightPaths.end(), std::back_inserter(result));
-    if (result.empty())
-    {
+    if (result.empty()) {
         result.push_back(std::vector<T>());
     }
 
@@ -450,16 +390,13 @@ std::vector<std::vector<T>> BSTree<T>::getNodePaths(const Node* node) const
 }
 
 template<typename T>
-void BSTree<T>::mirror()
-{
+void BSTree<T>::mirror() {
     mirrorNode(root);
 }
 
 template<typename T>
-void BSTree<T>::mirrorNode(Node* node)
-{
-    if (nullptr == node)
-    {
+void BSTree<T>::mirrorNode(Node* node) {
+    if (nullptr == node) {
         return;
     }
 
@@ -473,33 +410,27 @@ void BSTree<T>::mirrorNode(Node* node)
 }
 
 template<typename U>
-bool operator==(BSTree<U>& left, BSTree<U>& right)
-{
+bool operator==(BSTree<U>& left, BSTree<U>& right) {
     return left.isSameNodes(left.root, right.root);
 }
 
 template<typename U>
-bool operator!=(BSTree<U>& left, BSTree<U>& right)
-{
+bool operator!=(BSTree<U>& left, BSTree<U>& right) {
     return !(left == right);
 }
 
 template<typename T>
-bool BSTree<T>::isSameNodes(Node* lh, Node* rh)
-{
-    if (nullptr == lh)
-    {
+bool BSTree<T>::isSameNodes(Node* lh, Node* rh) {
+    if (nullptr == lh) {
         return nullptr == rh ? true : false;
     }
-    else if (nullptr == rh)
-    {
+    else if (nullptr == rh) {
         return false;
     }
 
     if (lh->getValue() == rh->getValue() &&
             isSameNodes(lh->getLeftNode(), rh->getLeftNode()) &&
-            isSameNodes(lh->getRightNode(), rh->getRightNode()))
-    {
+            isSameNodes(lh->getRightNode(), rh->getRightNode())) {
         return true;
     }
 
