2034987413e22d5e27f59a9e3d24537a149544b5
iamantony <antony.cherepanov@gmail.com>
2018-03-29 17:47:45 +0300

    Selection sort: change algorithm so it would be in-place

--- src/algorithms/sorting/selection_sort.hpp
+++ src/algorithms/sorting/selection_sort.hpp
@@ -7,37 +7,29 @@
 // Best case performance: O(n^2)
 // Worst case performance: O(n^2)
 // Worst Case Auxiliary Space Complexity: О(n) total, O(1) auxiliary
-// @input:
-// - container - object of container type
-// @output:
-// - T - sorted container
-template<typename T>
-T SelectionSort(const T& container)
-{
-    if (container.size() <= 1)
-    {
-        return container;
-    }
 
-    T array = container;
-    for (size_t i = 0; i < array.size() -1; ++i)
-    {
-        size_t minElemPos = i;
-        for (size_t j = i + 1; j < array.size(); ++j)
+namespace SSort {
+
+class Solution {
+public:
+    template<typename T>
+    void SelectionSort(T& container) {
+        for (size_t i = 0; i < container.size(); ++i)
         {
-            if (array[j] < array[minElemPos])
-            {
-                minElemPos = j;
+            size_t minElemPos = i;
+            for (size_t j = i + 1; j < container.size(); ++j) {
+                if (container[j] < container[minElemPos]) {
+                    minElemPos = j;
+                }
+            }
+
+            if (minElemPos != i) {
+                std::swap(container[i], container[minElemPos]);
             }
         }
-
-        if (minElemPos != i)
-        {
-            std::swap(array[i], array[minElemPos]);
-        }
     }
+};
 
-    return array;
 }
 
 #endif /* SELECTIONSORT_HPP_ */
