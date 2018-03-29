bbaa85c46400e71f0349ffb7ab2adfd2600cbe73
iamantony <antony.cherepanov@gmail.com>
2018-03-29 16:44:47 +0300

    Add file with test cases for sorting algorithms

---
+++ test/algorithms/sorting/testcases_for_sorting_algorithms.hpp
@@ -0,0 +1,16 @@
+#ifndef TESTCASES_FOR_SORTING_ALGORITHMS_HPP
+#define TESTCASES_FOR_SORTING_ALGORITHMS_HPP
+
+#include <vector>
+
+namespace TCFSortingAlgo {
+
+std::vector<int> EmptyContainer() {
+    return std::vector<int>();
+}
+
+}
+
+
+#endif // TESTCASES_FOR_SORTING_ALGORITHMS_HPP
+
