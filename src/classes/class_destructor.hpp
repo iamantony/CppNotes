18abae44dd0ed956dad3fb9963d7cdbb2f15c9f3
iamantony <antony.cherepanov@gmail.com>
2018-03-31 11:23:37 +0300

    Add new file for playing with class destructors

---
+++ src/classes/class_destructor.hpp
@@ -0,0 +1,57 @@
+#ifndef CLASS_DESTRUCTOR_HPP
+#define CLASS_DESTRUCTOR_HPP
+
+#include <iostream>
+
+// ==== 1. Function and object destruction
+// https://www.geeksforgeeks.org/playing-with-destructors-in-c/
+
+int globalVariable;
+
+class DestructingClass {
+public:
+    ~DestructingClass() {
+        std::cout << "Destructor of DestructingClass" << std::endl;
+        ::globalVariable = 10;
+    }
+};
+
+int functionThatChangeGlobaVar() {
+    globalVariable = 3;
+    DestructingClass obj;
+    return globalVariable;
+}
+
+int functionThatChangeGlobaVarFix() {
+    globalVariable = 3;
+
+    {
+        DestructingClass obj;
+    }
+
+    return globalVariable;
+}
+
+void FunctionAndDestructionOfObjects() {
+    /*
+    What we will get from functionThatChangeGlobaVar()?
+    Answer: 3. Why?
+    While returning from a function, destructor is the last method to be
+    executed. The destructor for the object “ob” is called after the value of
+    globalVariable is copied to the return value of the function.
+    */
+
+    std::cout << "FunctionAndDestructionOfObjects: globalVariable = " <<
+                 functionThatChangeGlobaVar() << std::endl;
+
+    /*
+    How to fix that? One of the solutions is to guarantee that destruction
+    of object will be called before return statement
+    */
+
+    std::cout << "FunctionAndDestructionOfObjects: globalVariable = " <<
+                 functionThatChangeGlobaVarFix() << std::endl;
+}
+
+#endif // CLASS_DESTRUCTOR_HPP
+
