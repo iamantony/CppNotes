79a6b665a0aaec0692c79b06ca4cf9dab25055de
iamantony <antony.cherepanov@gmail.com>
2018-04-03 19:20:57 +0300

    Make Couting sort stable

--- src/algorithms/sorting/counting_sort.hpp
+++ src/algorithms/sorting/counting_sort.hpp
@@ -1,12 +1,21 @@
 #ifndef COUNTING_SORT_HPP
 #define COUNTING_SORT_HPP
 
+// https://www.geeksforgeeks.org/counting-sort/
 // Counting sort is a sorting technique based on keys between a specific range.
 // It works by counting the number of objects having distinct key values
 // (kind of hashing). Then doing some arithmetic to calculate the position
 // of each object in the output sequence.
-// Time complexity: O(n)
-// Space complexity: O(n + M) where M is the max value of elements type
+// Time complexity: O(n + M) where M is the max value of elements type
+// Space complexity: O(n + M)
+
+// Points to be noted:
+// 1. Counting sort is efficient if the range of input data is not
+// significantly greater than the number of objects to be sorted. Consider
+// the situation where the input sequence is between range 1 to 10K and the
+// data is 10, 5, 10K, 5K.
+// 2. It is not a comparison based sorting. It running time complexity is O(n)
+// with space proportional to the range of data.
 
 #include <vector>
 #include <algorithm>
@@ -40,8 +49,9 @@ public:
         }
 
         // Create result sorted array
+        // We tranverse input array backwards to make algorithm stable
         std::vector<unsigned char> result(v.size(), 0);
-        for (size_t i = 0; i < v.size(); ++i) {
+        for (size_t i = v.size() - 1; ; --i) {
             // Find out position for value at v[i]. It will be the max current
             // position for such value
             size_t position = countVector[v[i]] - 1;
@@ -51,6 +61,12 @@ public:
 
             // Decrease position for the next element of v with the same value
             --countVector[v[i]];
+
+            // Exit for loop when we process last (in our case first in the
+            // input array) element
+            if (i == 0) {
+                break;
+            }
         }
 
         return result;
