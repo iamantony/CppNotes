c9fd51695941d344b162b5a5eb906111ae2e0d6b
iamantony <antony.cherepanov@gmail.com>
2018-04-12 09:36:56 +0300

    Rewrite quicksort

--- src/algorithms/sorting/quicksort.hpp
+++ src/algorithms/sorting/quicksort.hpp
@@ -2,11 +2,11 @@
 #define QUICKSORT_HPP_
 
 // Quick Sort Algorithm
-// Efficient, general-purpose, comparison-based sorting algorithm.
+// Efficient general-purpose in-place comparison-based sorting algorithm.
 // https://en.wikipedia.org/wiki/Quicksort
 // Best case performance: O(n log n) typical, O(n) natural variant
 // Worst case performance: O(n^2)
-// Worst Case Auxiliary Space Complexity: О(n)
+// Space Complexity: О(1)
 
 #include <limits>
 
@@ -28,41 +28,48 @@ private:
             return;
         }
 
-        // As pivot element choose middle element
-        size_t pivotIndex = start + (end - start)/ 2;
-        auto pivotValue = container[pivotIndex];
+        size_t pivotFinalIndex = Partition(container, start, end);
+        if (pivotFinalIndex > 0) {
+            Sort(container, start, pivotFinalIndex - 1);
+        }
 
-        size_t left = start, right = end;
-        do {
-            // Find element on the LEFT part of the container that is
-            // GREATER OR EQUAL than pivot element
-            while(container[left] < pivotValue) {
+        Sort(container, pivotFinalIndex + 1, end);
+    }
+
+    static size_t Partition(T& container,
+                            const size_t& start, const size_t& end) {
+        // Choose pivot element as middle element of container. Swap it
+        // with the first element.
+        size_t pivotIndex = start + (end - start)/ 2;
+        std::swap(container[pivotIndex], container[start]);
+
+        auto pivotValue = container[start];
+
+        // Let's iterate over container and compare elements with pivot.
+        // Our goal - is to rearrange elements in container so they would
+        // form two parts: first (left) part will contain only elements that
+        // are less than pivot; second (right) part will contain all other
+        // elements.
+        // We could start iteration with element that stands after pivot.
+        // We will use two indexes:
+        // 'left' - end of the partition with elements less than pivot
+        // 'i' - iterator over container elements
+        size_t left = start + 1;
+        for(size_t i = start + 1; i <= end; ++i)
+        {
+            // All elements that are less than pivot put to the left partition
+            if (container[i] < pivotValue) {
+                std::swap(container[i], container[left]);
                 ++left;
             }
-
-            // Find element on the RIGHT part of the container that is
-            // LESS OR EQUAL than pivot element
-            while(pivotValue < container[right]) {
-                --right;
-            }
-
-            if (left <= right) {
-                // Swap such elements so they will be in right parts of the
-                // container
-                std::swap(container[left], container[right]);
-                if (left < std::numeric_limits<size_t>::max()) {
-                    ++left;
-                }
-
-                if (std::numeric_limits<size_t>::min() < right) {
-                    --right;
-                }
-            }
         }
-        while(left <= right);
 
-        Sort(container, start, right);
-        Sort(container, left, end);
+        // Put pivot element to its final position - last element of partition
+        // that contains only elements that are less than pivot
+        std::swap(container[start], container[left - 1]);
+
+        // Return final position of pivot element
+        return left - 1;
     }
 };
 
