89cac515b4a8cf9a1126ec73dae4254cd8693016
iamantony <antony.cherepanov@gmail.com>
2018-05-22 11:53:04 +0300

    Issue 23: fix merge sort
    - use O(n) additional space on each step instead of O(2n)
    - in-place sorting

--- src/algorithms/sorting/merge_sort.hpp
+++ src/algorithms/sorting/merge_sort.hpp
@@ -4,6 +4,7 @@
 // Merge Sort Algorithm
 // Efficient, general-purpose, comparison-based sorting algorithm.
 // https://en.wikipedia.org/wiki/Merge_sort
+// https://www.geeksforgeeks.org/merge-sort/
 // Best case performance: O(n log n) typical, O(n) natural variant
 // Worst case performance: O(n log n)
 // Worst Case Auxiliary Space Complexity: О(n)
@@ -15,62 +16,68 @@ namespace MS {
 template<typename T>
 class Solution {
 public:
-    static T MergeSort(const T& container) {
-        return SortImpl(container);
+    static void MergeSort(T& container) {
+        if (container.size() < 2) {
+            return;
+        }
+
+        MergeSortImpl(container, 0, container.size() - 1);
     }
 
 private:
-    static T SortImpl(const T& input) {
-        if(input.size() < 2) {
-            return input;
+    static void MergeSortImpl(T& container,
+                              const size_t& start, const size_t& end)
+    {
+        if(start >= end) {
+            return;
         }
 
-        // Find middle element
-        size_t middle = input.size()/2;
-        typename T::const_iterator middleIter(input.cbegin());
-        std::advance(middleIter, middle);
+        size_t middle = start + (end - start) / 2;
+        MergeSortImpl(container, start, middle);
+        MergeSortImpl(container, middle + 1, end);
 
-        // Create two containers that contains halfs of the original container
-        T first(input.begin(), middleIter);
-        T second(middleIter, input.end());
-
-        first = SortImpl(first);
-        second = SortImpl(second);
-        return Merge(first, second);
+        return Merge(container, start, middle, end);
     }
 
-    static T Merge(const T& first, const T& second) {
-        if (first.empty() && second.empty()) {
-            return T();
-        }
-        else if (first.empty() && false == second.empty()) {
-            return second;
-        }
-        else if (false == first.empty() && second.empty()) {
-            return first;
+    static void Merge(T& container, const size_t& start,
+                      const size_t& middle, const size_t& end)
+    {
+        if (start > middle || middle > end) {
+            return;
         }
 
-        T result;
+        // Create two containers that would contain values from [start, middle]
+        // and [middle + 1, end]
+        T leftContainer, rightContainer;
+        {
+            auto createIterator = [](const T& array, const size_t& position) {
+                auto iter = array.begin();
+                std::advance(iter, position);
+                return iter;
+            };
 
-        // Just setup size of result container as sum of sizes of merging
-        // containers
-        std::copy(first.begin(), first.end(), back_inserter(result));
-        std::copy(second.begin(), second.end(), back_inserter(result));
+            auto startIter = createIterator(container, start);
+            auto middleIter = createIterator(container, middle + 1);
+            auto endIter = createIterator(container, end + 1);
+
+            std::copy(startIter, middleIter, back_inserter(leftContainer));
+            std::copy(middleIter, endIter, back_inserter(rightContainer));
+        }
 
         // Merge two containers
-        for (size_t k = 0, i = 0, j = 0; k < result.size(); ++k) {
-            if (i < first.size() &&
-                        (second.size() <= j || first[i] <= second[j])) {
-                result[k] = first[i];
-                ++i;
+        for (size_t c = start, lc = 0, rc = 0; c <= end; ++c) {
+            if (lc < leftContainer.size() &&
+                        (rightContainer.size() <= rc ||
+                         leftContainer[lc] <= rightContainer[rc]))
+            {
+                container[c] = leftContainer[lc];
+                ++lc;
             }
             else {
-                result[k] = second[j];
-                ++j;
+                container[c] = rightContainer[rc];
+                ++rc;
             }
         }
-
-        return result;
     }
 };
 
--- test/algorithms/sorting/test_merge_sort.hpp
+++ test/algorithms/sorting/test_merge_sort.hpp
@@ -11,57 +11,57 @@ using namespace TCFSortingAlgo;
 BOOST_AUTO_TEST_CASE(test_ms_empty_container)
 {
     std::vector<int> container = EmptyContainer();
-    std::vector<int> result = MS::Solution<std::vector<int>>::MergeSort(container);
-    BOOST_CHECK(std::vector<int>() == result);
+    MS::Solution<std::vector<int>>::MergeSort(container);
+    BOOST_CHECK(std::vector<int>() == container);
 }
 
 BOOST_AUTO_TEST_CASE(test_ms_one_value)
 {
     std::vector<int> container = ContainerWithOneValue();
-    std::vector<int> result = MS::Solution<std::vector<int>>::MergeSort(container);
-    BOOST_CHECK(std::is_sorted(result.begin(), result.end()));
+    MS::Solution<std::vector<int>>::MergeSort(container);
+    BOOST_CHECK(std::is_sorted(container.begin(), container.end()));
 }
 
 BOOST_AUTO_TEST_CASE(test_ms_sorted_container)
 {
     std::vector<int> container = SortedContainer();
-    std::vector<int> result = MS::Solution<std::vector<int>>::MergeSort(container);
-    BOOST_CHECK(std::is_sorted(result.begin(), result.end()));
+    MS::Solution<std::vector<int>>::MergeSort(container);
+    BOOST_CHECK(std::is_sorted(container.begin(), container.end()));
 }
 
 BOOST_AUTO_TEST_CASE(test_ms_min_max_int_values)
 {
     std::vector<int> container = ContainerWithMinAndMaxValues();
-    std::vector<int> result = MS::Solution<std::vector<int>>::MergeSort(container);
-    BOOST_CHECK(std::is_sorted(result.begin(), result.end()));
+    MS::Solution<std::vector<int>>::MergeSort(container);
+    BOOST_CHECK(std::is_sorted(container.begin(), container.end()));
 }
 
 BOOST_AUTO_TEST_CASE(test_ms_positive_negative_values)
 {
     std::vector<int> container = ContainerWithPositiveAndNegativeValues();
-    std::vector<int> result = MS::Solution<std::vector<int>>::MergeSort(container);
-    BOOST_CHECK(std::is_sorted(result.begin(), result.end()));
+    MS::Solution<std::vector<int>>::MergeSort(container);
+    BOOST_CHECK(std::is_sorted(container.begin(), container.end()));
 }
 
 BOOST_AUTO_TEST_CASE(test_ms_duplicate_values)
 {
     std::vector<int> container = ContainerWithDuplicates();
-    std::vector<int> result = MS::Solution<std::vector<int>>::MergeSort(container);
-    BOOST_CHECK(std::is_sorted(result.begin(), result.end()));
+    MS::Solution<std::vector<int>>::MergeSort(container);
+    BOOST_CHECK(std::is_sorted(container.begin(), container.end()));
 }
 
 BOOST_AUTO_TEST_CASE(test_ms_many_values)
 {
     std::vector<int> container = ContainerWithManyValues();
-    std::vector<int> result = MS::Solution<std::vector<int>>::MergeSort(container);
-    BOOST_CHECK(std::is_sorted(result.begin(), result.end()));
+    MS::Solution<std::vector<int>>::MergeSort(container);
+    BOOST_CHECK(std::is_sorted(container.begin(), container.end()));
 }
 
 BOOST_AUTO_TEST_CASE(test_ms_random_values)
 {
     for (int i = 0; i < 10; ++i) {
         std::vector<int> container = RandomContainer();
-        std::vector<int> result = MS::Solution<std::vector<int>>::MergeSort(container);
+        MS::Solution<std::vector<int>>::MergeSort(container);
         BOOST_CHECK(std::is_sorted(container.begin(), container.end()));
     }
 }
