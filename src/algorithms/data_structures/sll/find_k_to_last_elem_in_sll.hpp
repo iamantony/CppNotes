d2da81d857b38182a858eba6983b830e6fdce30c
iamantony <antony.cherepanov@gmail.com>
2018-06-11 12:11:16 +0300

    Apply new naming approach to algo Find k-th to last element in SLL

--- src/algorithms/data_structures/sll/find_k_to_last_elem_in_sll.hpp
+++ src/algorithms/data_structures/sll/find_k_to_last_elem_in_sll.hpp
@@ -10,63 +10,64 @@
 
 #include "types/ds/singly_linked_list_nodes.hpp"
 
-template<typename T>
-NodeSLL<T>* FindKToLastElemRecursion(NodeSLL<T>* head, const int& k)
-{
-    std::function<NodeSLL<T>*(NodeSLL<T>*, const int&, int&)> findElem;
-    findElem = [&findElem](NodeSLL<T>* node, const int& kth, int& i) -> NodeSLL<T>*
+namespace Algo { namespace DS { namespace SLL {
+
+class FindKthToLast {
+public:
+    template<typename T>
+    static NodeSLL<T>* FindRecursive(NodeSLL<T>* head, const int& k)
     {
-        if (nullptr == node)
+        std::function<NodeSLL<T>*(NodeSLL<T>*, const int&, int&)> findElem;
+        findElem = [&findElem](NodeSLL<T>* node, const int& kth, int& i) -> NodeSLL<T>*
         {
+            if (nullptr == node) {
+                return nullptr;
+            }
+
+            NodeSLL<T>* foundNode = findElem(node->next, kth, i);
+            ++i;
+            if (i == kth) {
+                // This is k-th to last element
+                return node;
+            }
+
+            return foundNode;
+        };
+
+        int i = 0;
+        return findElem(head, k, i);
+    }
+
+    template<typename T>
+    static NodeSLL<T>* FindIterative(NodeSLL<T>* head, const int& k)
+    {
+        if (nullptr == head) {
             return nullptr;
         }
 
-        NodeSLL<T>* foundNode = findElem(node->next, kth, i);
-        ++i;
-        if (i == kth)
-        {
-            // This is k-th to last element
-            return node;
+        NodeSLL<T>* p1 = head;
+        NodeSLL<T>* p2 = head;
+
+        // Move second pointer to K position
+        for (int i = 0; i < k - 1; ++i) {
+            if (nullptr == p2->next) {
+                return nullptr;
+            }
+
+            p2 = p2->next;
         }
 
-        return foundNode;
-    };
-
-    int i = 0;
-    return findElem(head, k, i);
-}
-
-template<typename T>
-NodeSLL<T>* FindKToLastElemIteration(NodeSLL<T>* head, const int& k)
-{
-    if (nullptr == head)
-    {
-        return nullptr;
-    }
-
-    NodeSLL<T>* p1 = head;
-    NodeSLL<T>* p2 = head;
-
-    // Move second pointer to K position
-    for (int i = 0; i < k -1; ++i)
-    {
-        if (nullptr == p2->next)
-        {
-            return nullptr;
+        // Move both pointers till p2 will not get to the end. p1 will point to
+        // k-th to last element
+        while(nullptr != p2->next) {
+            p2 = p2->next;
+            p1 = p1->next;
         }
 
-        p2 = p2->next;
+        return p1;
     }
+};
 
-    // Move both pointers till p2 will not get to the end. p1 will point to
-    // k-th to last element
-    while(nullptr != p2->next)
-    {
-        p2 = p2->next;
-        p1 = p1->next;
-    }
-
-    return p1;
-}
+} } }
 
 #endif /* FIND_K_TO_LAST_ELEM_IN_SLL_HPP_ */
--- test/algorithms/data_structures/sll/test_find_k_to_last_elem_in_sll.hpp
+++ test/algorithms/data_structures/sll/test_find_k_to_last_elem_in_sll.hpp
@@ -9,18 +9,16 @@ BOOST_AUTO_TEST_CASE(test_fkesll_short_sll)
 {
     NodeSLL<int>* sll = new NodeSLL<int>(0);
     AppendToSLL(&sll, 1);
-
-    BOOST_CHECK(nullptr == FindKToLastElemRecursion(sll, 5));
-    BOOST_CHECK(nullptr == FindKToLastElemIteration(sll, 5));
+    BOOST_CHECK(nullptr == Algo::DS::SLL::FindKthToLast::FindRecursive(sll, 5));
+    BOOST_CHECK(nullptr == Algo::DS::SLL::FindKthToLast::FindIterative(sll, 5));
     DeleteSLL(&sll);
 }
 
 BOOST_AUTO_TEST_CASE(test_fkesll_sll_k_size)
 {
     NodeSLL<int>* sll = CreateSLL<int>({0, 1, 2, 3});
-
-    BOOST_CHECK(sll == FindKToLastElemRecursion(sll, 4));
-    BOOST_CHECK(sll == FindKToLastElemIteration(sll, 4));
+    BOOST_CHECK(sll == Algo::DS::SLL::FindKthToLast::FindRecursive(sll, 4));
+    BOOST_CHECK(sll == Algo::DS::SLL::FindKthToLast::FindIterative(sll, 4));
     DeleteSLL(&sll);
 }
 
@@ -29,13 +27,12 @@ BOOST_AUTO_TEST_CASE(test_fkesll_long_sll)
     NodeSLL<int>* sll = CreateSLL<int>({0, 1, 2, 3, 4, 5});
 
     NodeSLL<int>* expected = sll;
-    while(expected != nullptr && expected->value != 4)
-    {
+    while(expected != nullptr && expected->value != 4) {
         expected = expected->next;
     }
 
-    BOOST_CHECK(expected == FindKToLastElemRecursion(sll, 2));
-    BOOST_CHECK(expected == FindKToLastElemIteration(sll, 2));
+    BOOST_CHECK(expected == Algo::DS::SLL::FindKthToLast::FindRecursive(sll, 2));
+    BOOST_CHECK(expected == Algo::DS::SLL::FindKthToLast::FindIterative(sll, 2));
     DeleteSLL(&sll);
 }
 
