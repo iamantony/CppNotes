ee607959f0873bb906688a10e7af258d635d594d
iamantony <antony.cherepanov@gmail.com>
2018-06-20 13:50:07 +0300

    Pass to a thread function of the object

--- src/threads/associate_thread_of_execution.hpp
+++ src/threads/associate_thread_of_execution.hpp
@@ -10,9 +10,11 @@ void func() {
     std::cout << std::this_thread::get_id() << " In func()" << std::endl;
 }
 
-void funcWithArgs(int x, int y) {
+void funcWithArgs(int x, int& y) {
     std::cout << std::this_thread::get_id() << " In funcWithArgs(): " <<
               x << " , " << y << std::endl;
+
+    ++y;
 }
 
 class Example {
@@ -28,12 +30,34 @@ private:
     int m_value = 0;
 };
 
+class Worker {
+public:
+    void DoWork(int no, int yes) {
+        if (no < yes) {
+            std::cout << std::this_thread::get_id() <<
+                         " In DoWork(): I'm gonna show you my precious: " <<
+                         m_precious << std::endl;
+        }
+        else {
+            std::cout << std::this_thread::get_id() <<
+                         " In Example(): sorry, I'm poor" << std::endl;
+        }
+    }
+
+private:
+    int m_precious = 2^10;
+};
 
 void StartExamplesOfHowToAssociateThreadOfExecution() {
     std::thread t1(func);
-    std::thread t2(funcWithArgs, 10, 11);
+
+    int mainY = 11;
+    std::thread t2(funcWithArgs, 10, std::ref(mainY));
     std::thread t3(Example(42), -5);
-    std::thread t4([]() {
+
+    Worker workerObject;
+    std::thread t4(&Worker::DoWork, &workerObject, 0, 1);
+    std::thread t5([]() {
         std::cout << std::this_thread::get_id() << " In lambda" << std::endl;
     });
 
@@ -41,6 +65,9 @@ void StartExamplesOfHowToAssociateThreadOfExecution() {
     t2.join();
     t3.join();
     t4.join();
+    t5.join();
+
+    std::cout << "In main thread mainY = " << mainY << std::endl;
 }
 
 }
