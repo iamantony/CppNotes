d8093b013b88aea6ebd17227e083d29680c3643c
iamantony <antony.cherepanov@gmail.com>
2018-07-05 20:58:57 +0300

    Minor changes in Nodes Deep Copy

--- src/algorithms/data_structures/nodes_deep_copy.hpp
+++ src/algorithms/data_structures/nodes_deep_copy.hpp
@@ -5,21 +5,19 @@
 // returns a complete copy of the passed in data structure. The Node data
 // structure contains two pointers to other Nodes.
 
-class Node
-{
+namespace Algo::DS {
+
+class Node {
 public:
     Node(const Node* first, const Node* second) :
-        m_first(first), m_second(second) {}
+        m_first(first), m_second(second) { }
 
-    ~Node()
-    {
-        if (nullptr != m_first)
-        {
+    ~Node() {
+        if (nullptr != m_first) {
             delete m_first;
         }
 
-        if (nullptr != m_second)
-        {
+        if (nullptr != m_second) {
             delete m_second;
         }
     }
@@ -28,25 +26,23 @@ public:
     Node* m_second = nullptr;
 };
 
-Node* DeepCopyOfNode(const Node* const node)
-{
-    if (nullptr == node)
-    {
+Node* DeepCopyOfNode(const Node* const node) {
+    if (nullptr == node) {
         return nullptr;
     }
 
     Node* result = new Node;
-    if (nullptr != node->m_first)
-    {
+    if (nullptr != node->m_first) {
         result->m_first = DeepCopyOfNode(node->m_first);
     }
 
-    if (nullptr != node->m_second)
-    {
+    if (nullptr != node->m_second) {
         result->m_second = DeepCopyOfNode(node->m_second);
     }
 
     return result;
+}
+
 }
 
 #endif /* NODES_DEEP_COPY_HPP_ */
