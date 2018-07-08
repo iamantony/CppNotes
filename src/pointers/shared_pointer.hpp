edd483cc5fd7015d07edfdc16bedd5c3924ddd3d
iamantony <antony.cherepanov@gmail.com>
2018-07-08 12:15:35 +0300

    Add second implementation of shared pointer

--- src/pointers/shared_pointer.hpp
+++ src/pointers/shared_pointer.hpp
@@ -19,7 +19,6 @@ template<typename T>
 class SharedPointer
 {
     class RefCounter {
-    private:
         T* obj;
         int counter;
         std::mutex mutex;
@@ -57,6 +56,7 @@ class SharedPointer
             if (counter <= 0 && obj != nullptr) {
                 delete obj;
                 obj = nullptr;
+                counter = 0;
             }
         }
     };
@@ -68,7 +68,7 @@ public:
     SharedPointer() : rc(nullptr) {}
 
     // Constructor with T* arg
-    explicit SharedPointer(T* ptr) {
+    explicit SharedPointer(T* ptr) : rc(nullptr) {
         if (nullptr != ptr) {
             // We could not have enough memory to create new RefCounter,
             // so we should catch exception, delete ptr (or it will be
@@ -162,6 +162,106 @@ private:
             delete rc;
             rc = nullptr;
         }
+    }
+};
+
+template <typename T>
+class SharedPointer2 {
+    T* object = nullptr;
+    int* counter = nullptr;
+
+public:
+    // Default constructor
+    SharedPointer2() { }
+
+    // Explicit constructor that get pointer to some object that we want to
+    // share
+    explicit SharedPointer2(T* obj) {
+        if (obj != nullptr) {
+            // If we would not have enough memory to create new counter, we
+            // will get std::bad_alloc exception. So in catch() section
+            // we should delete 'obj' (or it will be leaked)
+            // and rethrow exception
+            try {
+                counter = new int(1);
+            }
+            catch(...) {
+                delete obj;
+                throw;
+            }
+
+            object = obj;
+        }
+    }
+
+    // Copy constructor
+    SharedPointer2(const SharedPointer2<T>& other) {
+        object = other.object;
+        counter = other.counter;
+        if (counter) {
+            ++(*counter);
+        }
+    }
+
+    // Copy assignment operator
+    // Here we implicitly create copy of the some other SharedPointer2 into
+    // function argument. Next we just swap class members. In destructor of
+    // argument object our old 'counter' will be decreased and object will be
+    // removed, if necessary
+    SharedPointer2<T>& operator=(SharedPointer2<T> other) {
+        other.swap(*this);
+        return *this;
+    }
+
+    // Move constructor
+    // Move semantics say that the source object may be left in an undefined
+    // (but must be valid) state. So the easiest way to implement this is
+    // simply to swap the state of the current object with the source object
+    // (we know our state is valid so just swap it with the incoming object
+    // state (its destructor will then take care of destroying the pointers
+    // we are holding)).
+    SharedPointer2(SharedPointer2<T>&& other) {
+        other.swap(*this);
+    }
+
+    // Move assignment operator
+    SharedPointer2<T>& operator=(const SharedPointer2<T>&& other) {
+        other.swap(*this);
+        return *this;
+    }
+
+    // Destructor
+    ~SharedPointer2() {
+        if (nullptr == counter) {
+            return;
+        }
+
+        --(*counter);
+        if (*counter <= 0 && object != nullptr) {
+            delete object;
+            delete counter;
+            object = nullptr;
+            counter = nullptr;
+        }
+    }
+
+    void swap(SharedPointer2<T>& other) {
+        std::swap(object, other.object);
+        std::swap(counter, other.counter);
+    }
+
+    // Inside these access functions we don't change 'object', so we could
+    // define them as const methods
+    T& operator*() const {
+        return *Get();
+    }
+
+    T* operator->() const {
+        return Get();
+    }
+
+    T* Get() const {
+        return object;
     }
 };
 
--- test/pointers/test_shared_pointer.hpp
+++ test/pointers/test_shared_pointer.hpp
@@ -31,32 +31,36 @@ public:
 
 BOOST_AUTO_TEST_CASE(test_shrdptr_nullptr)
 {
-    Pointers::SharedPointer<MyInt> ptr1;
-    Pointers::SharedPointer<MyInt> ptr2(nullptr);
-    Pointers::SharedPointer<MyInt> ptr3(ptr1);
+    Pointers::SharedPointer2<MyInt> ptr1;
+    Pointers::SharedPointer2<MyInt> ptr2(nullptr);
+    Pointers::SharedPointer2<MyInt> ptr3(ptr1);
 }
 
 BOOST_AUTO_TEST_CASE(test_shrdptr_create_one_sp)
 {
-    Pointers::SharedPointer<MyInt> ptr1(new MyInt(42));
+    Pointers::SharedPointer2<MyInt> ptr1(new MyInt(42));
 }
 
 BOOST_AUTO_TEST_CASE(test_shrdptr_sp_copy_constr)
 {
-    Pointers::SharedPointer<MyInt> ptr1(new MyInt(43));
-    Pointers::SharedPointer<MyInt> ptr2(ptr1);
+    Pointers::SharedPointer2<MyInt> ptr1(new MyInt(43));
+    {
+        Pointers::SharedPointer2<MyInt> ptr2(ptr1);
+    }
 }
 
 BOOST_AUTO_TEST_CASE(test_shrdptr_sp_copy_assign)
 {
-    Pointers::SharedPointer<MyInt> ptr1(new MyInt(44));
-    Pointers::SharedPointer<MyInt> ptr2 = ptr1;
+    Pointers::SharedPointer2<MyInt> ptr1(new MyInt(44));
+    Pointers::SharedPointer2<MyInt> ptr2 = ptr1;
 }
 
 BOOST_AUTO_TEST_CASE(test_shrdptr_sp_move_constr)
 {
-    Pointers::SharedPointer<MyInt> ptr1(
-                Pointers::SharedPointer<MyInt>(new MyInt(45)));
+    Pointers::SharedPointer2<MyInt> ptr1(
+                Pointers::SharedPointer2<MyInt>(new MyInt(45)));
+    ptr1.Get()->value = 46;
+    std::cout << "Updated value: " << (*ptr1).value << std::endl;
 }
 
 #endif // TEST_SHARED_POINTER_HPP
