ea38539f9a842924a95b9cbe59d7c5a6a0188f96
iamantony <antony.cherepanov@gmail.com>
2018-08-01 10:42:28 +0300

    Refactoring of Circular Queue

--- src/types/ds/circular_queue.hpp
+++ src/types/ds/circular_queue.hpp
@@ -1,14 +1,17 @@
+#ifndef CIRCULAR_QUEUE_HPP
+#define CIRCULAR_QUEUE_HPP
+
 /*
 https://leetcode.com/problems/design-circular-queue/description/
-Design your implementation of the circular queue. The circular queue is 
-a linear data structure in which the operations are performed based on 
-FIFO (First In First Out) principle and the last position is connected 
+Design your implementation of the circular queue. The circular queue is
+a linear data structure in which the operations are performed based on
+FIFO (First In First Out) principle and the last position is connected
 back to the first position to make a circle. It is also called "Ring Buffer".
 
-One of the benefits of the circular queue is that we can make use of 
-the spaces in front of the queue. In a normal queue, once the queue 
-becomes full, we cannot insert the next element even if there is a 
-space in front of the queue. But using the circular queue, we can 
+One of the benefits of the circular queue is that we can make use of
+the spaces in front of the queue. In a normal queue, once the queue
+becomes full, we cannot insert the next element even if there is a
+space in front of the queue. But using the circular queue, we can
 use the space to store new values.
 
 Your implementation should support following operations:
@@ -16,13 +19,13 @@ Your implementation should support following operations:
 MyCircularQueue(k): Constructor, set the size of the queue to be k.
 Front: Get the front item from the queue. If the queue is empty, return -1.
 Rear: Get the last item from the queue. If the queue is empty, return -1.
-enQueue(value): Insert an element into the circular queue. Return 
+enQueue(value): Insert an element into the circular queue. Return
 true if the operation is successful.
-deQueue(): Delete an element from the circular queue. Return true 
+deQueue(): Delete an element from the circular queue. Return true
 if the operation is successful.
 isEmpty(): Checks whether the circular queue is empty or not.
 isFull(): Checks whether the circular queue is full or not.
- 
+
 
 Example:
 
@@ -36,7 +39,7 @@ circularQueue.isFull();  // return true
 circularQueue.deQueue();  // return true
 circularQueue.enQueue(4);  // return true
 circularQueue.Rear();  // return 4
- 
+
 Note:
 
 All values will be in the range of [0, 1000].
@@ -44,92 +47,95 @@ The number of operations will be in the range of [1, 1000].
 Please do not use the built-in Queue library.
 */
 
-// TODO: refactoring, add tests
-class MyCircularQueue {
-    std::vector<int> m_v;
-    // Position with first element
-    size_t start = 0;
+#include <vector>
+#include <exception>
+
+namespace Types::DS {
+
+// TODO: add tests
+template <typename T>
+class CircularQueue {
+    std::vector<T> m_data;
+    bool m_full = false;
+    // Position of first element
+    size_t m_start = 0;
     // Position one past the last element
-    size_t end = 0;
-    bool full = false;
-    
+    size_t m_end = 0;
+
 public:
-    /** Initialize your data structure here. Set the size of the queue to be k. */
-    MyCircularQueue(int k) {
-        if (k <= 0) {
-            throw std::invalid_argument("Invalid arg");
-        }
-        
-        m_v.resize(static_cast<size_t>(k), 0);
+    CircularQueue(const size_t& size) {
+        m_data.resize(size, 0);
     }
-    
-    /** Insert an element into the circular queue. Return true if the operation is successful. */
-    bool enQueue(int value) {
-        if (isFull()) {
+
+    bool push(const T& value) {
+        if (m_data.size() == 0 || isFull()) {
             return false;
         }
-        
+
         // Check base case - queue is empty
-        if (start == end) {
-            m_v[start] = value;
-            end = 1;
+        if (m_start == m_end) {
+            m_data[m_start] = value;
+            m_end = 1;
         }
         else {
-            m_v[end++] = value;
-            end = (end >= m_v.size()) ? 0 : end;
-            
-            if (start == end) {
-                full = true;
+            // Otherwise insert element to the end of the queue
+            m_data[m_end++] = value;
+
+            // Check if we need to move end position to the start of the vector
+            m_end = (m_end >= m_data.size()) ? 0 : m_end;
+
+            // Check if queue become full
+            if (m_start == m_end) {
+                m_full = true;
             }
         }
-        
+
         return true;
     }
-    
-    /** Delete an element from the circular queue. Return true if the operation is successful. */
-    bool deQueue() {
+
+    bool pop() {
         if (isEmpty()) {
             return false;
         }
-        
-        ++start;
-        full = false;
-        if (start >= m_v.size()) {
-            start = 0;
+
+        ++m_start;
+        m_full = false;
+        if (m_start >= m_data.size()) {
+            m_start = 0;
         }
-        
-        if (start == end) {
-            start = end = 0;
+
+        if (m_start == m_end) {
+            m_start = m_end = 0;
         }
-        
+
         return true;
     }
-    
-    /** Get the front item from the queue. */
-    int Front() {
+
+    T front() {
         if (isEmpty()) {
-            return -1;
+            throw std::logic_error("Circular queue is empty");
         }
-        
-        return m_v[start];
+
+        return m_data[m_start];
     }
-    
-    /** Get the last item from the queue. */
-    int Rear() {
+
+    T back() {
         if (isEmpty()) {
-            return -1;
+            throw std::logic_error("Circular queue is empty");
         }
-        
-        return (end > 0) ? m_v[end - 1] : m_v[m_v.size() - 1];
+
+        return (m_end > 0) ? m_data[m_end - 1] : m_data[m_data.size() - 1];
     }
-    
-    /** Checks whether the circular queue is empty or not. */
+
     bool isEmpty() {
-        return !isFull() && start == end;
+        return m_data.size() == 0 || (!isFull() && m_start == m_end);
     }
-    
-    /** Checks whether the circular queue is full or not. */
+
     bool isFull() {
-        return full;
+        return m_data.size() == 0 || m_full;
     }
 };
+
+}
+
+#endif // CIRCULAR_QUEUE_HPP
