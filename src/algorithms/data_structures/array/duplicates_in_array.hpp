0570c54d20eacfd528268eb72f38667de782f114
iamantony <antony.cherepanov@gmail.com>
2018-08-30 19:24:02 +0300

    Add algo - Check if array contains duplicate elements within distance

--- src/algorithms/data_structures/array/duplicates_in_array.hpp
+++ src/algorithms/data_structures/array/duplicates_in_array.hpp
@@ -1,25 +1,62 @@
 #ifndef DUPLICATES_IN_ARRAY_HPP
 #define DUPLICATES_IN_ARRAY_HPP
 
-// https://leetcode.com/problems/contains-duplicate/description/
+/*
+https://leetcode.com/problems/contains-duplicate/description/
+Given an array of integers, find if the array contains any duplicates.
+Your function should return true if any value appears at least twice in
+the array, and it should return false if every element is distinct.
 
-// Given an array of integers, find if the array contains any duplicates.
-// Your function should return true if any value appears at least twice in
-// the array, and it should return false if every element is distinct.
+
+https://leetcode.com/problems/contains-duplicate-ii/description/
+Given an array of integers and an integer k, find out whether there are two
+distinct indices i and j in the array such that nums[i] = nums[j] and the
+absolute difference between i and j is at most k.
+
+Example 1:
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+Example 2:
+Input: nums = [1,0,1,1], k = 1
+Output: true
+
+Example 3:
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+*/
 
 #include <vector>
+#include <unordered_map>
 #include <algorithm>
 
 namespace Algo::DS::Array {
 
 class DuplicatesInArray {
 public:
-    static bool containsDuplicates(std::vector<int>& nums) {
+    static bool Contains(std::vector<int>& nums) {
         std::sort(nums.begin(), nums.end());
         for (size_t i = 1; i < nums.size(); ++i) {
             if (nums[i] == nums[i - 1]) {
                 return true;
             }
+        }
+
+        return false;
+    }
+
+    static bool ContainsWithinDistance(
+            const std::vector<int>& nums, const size_t& maxDistance)
+    {
+        std::unordered_map<int, size_t> elements;
+        for (size_t i = 0; i < nums.size(); ++i) {
+            if (elements.count(nums[i]) > 0) {
+                if (i - elements[nums[i]] <= maxDistance) {
+                    return true;
+                }
+            }
+
+            elements[nums[i]] = i;
         }
 
         return false;
--- test/algorithms/data_structures/array/test_duplicates_in_array.hpp
+++ test/algorithms/data_structures/array/test_duplicates_in_array.hpp
@@ -7,20 +7,54 @@
 
 BOOST_AUTO_TEST_CASE(test_dia_empty_arr)
 {
-    std::vector<int> numbers = {};
-    BOOST_CHECK(!Algo::DS::Array::DuplicatesInArray::containsDuplicates(numbers));
+    {
+        std::vector<int> numbers = {};
+        BOOST_CHECK(!Algo::DS::Array::DuplicatesInArray::Contains(numbers));
+    }
+
+    {
+        std::vector<int> numbers = {};
+        size_t distance = 1;
+        BOOST_CHECK(!Algo::DS::Array::DuplicatesInArray::ContainsWithinDistance(
+                        numbers, distance));
+    }
 }
 
 BOOST_AUTO_TEST_CASE(test_dia_no_duplicates)
 {
-    std::vector<int> numbers = {1, 2, 3, 4, 5, 6};
-    BOOST_CHECK(!Algo::DS::Array::DuplicatesInArray::containsDuplicates(numbers));
+    {
+        std::vector<int> numbers = {1, 2, 3, 4, 5, 6};
+        BOOST_CHECK(!Algo::DS::Array::DuplicatesInArray::Contains(numbers));
+    }
+
+    {
+        std::vector<int> numbers = {1, 2, 3, 4, 5, 6};
+        size_t distance = 1;
+        BOOST_CHECK(!Algo::DS::Array::DuplicatesInArray::ContainsWithinDistance(
+                        numbers, distance));
+    }
+
+    {
+        std::vector<int> numbers = {1, 2, 3, 4, 5, 3};
+        size_t distance = 4;
+        BOOST_CHECK(Algo::DS::Array::DuplicatesInArray::ContainsWithinDistance(
+                        numbers, distance));
+    }
 }
 
 BOOST_AUTO_TEST_CASE(test_dia_have_duplicates)
 {
-    std::vector<int> numbers = {1, 2, 3, 4, 5, 3};
-    BOOST_CHECK(Algo::DS::Array::DuplicatesInArray::containsDuplicates(numbers));
+    {
+        std::vector<int> numbers = {1, 2, 3, 4, 5, 3};
+        BOOST_CHECK(Algo::DS::Array::DuplicatesInArray::Contains(numbers));
+    }
+
+    {
+        std::vector<int> numbers = {1, 2, 3, 4, 5, 3};
+        size_t distance = 3;
+        BOOST_CHECK(Algo::DS::Array::DuplicatesInArray::ContainsWithinDistance(
+                        numbers, distance));
+    }
 }
 
 #endif // TEST_DUPLICATES_IN_ARRAY_HPP
