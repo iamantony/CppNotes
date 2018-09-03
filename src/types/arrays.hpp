02303d595bb3ef32f03f95c84ba2736a3e809df9
iamantony <antony.cherepanov@gmail.com>
2018-09-03 15:20:30 +0300

    Example: access element of multidimensional array

--- src/types/arrays.hpp
+++ src/types/arrays.hpp
@@ -8,7 +8,7 @@
 // https://www.geeksforgeeks.org/g-fact/
 // https://www.geeksforgeeks.org/difference-pointer-array-c/
 
-namespace Arrays {
+namespace Types::Arrays {
 
 void PrintArray(int* arr, const size_t& size, const std::string& info) {
     for (size_t i = 0; i < size; ++i) {
@@ -69,10 +69,79 @@ void PointerVsArray() {
     // arr = someOtherPtr;
 }
 
+void AccessElementIn2DArray() {
+    /*
+     https://www.geeksforgeeks.org/multidimensional-pointer-arithmetic-in-cc/amp/
+
+    In C/C++, arrays and pointers have similar semantics, except on type
+    information.
+
+    As an example, given a 3D array
+
+    int buffer[5][7][6];
+
+    An element at location [2][1][2] can be accessed as “buffer[2][1][2]” or
+    *( *( *(buffer + 2) + 1) + 2).
+
+    Observe the following declaration:
+    T *p; // p is a pointer to an object of type T
+
+    When a pointer p is pointing to an object of type T, the expression *p is
+    of type T. For example buffer is of type array of 5 two dimensional arrays.
+    The type of the expression *buffer is “array of arrays (i.e.
+    two dimensional array)”.
+
+    Based on the above concept translating the expression
+    *( *( *(buffer + 2) + 1) + 2) step-by-step makes it more clear.
+
+    1. buffer – An array of 5 two dimensional arrays, i.e. its type is
+    “array of 5 two dimensional arrays”.
+
+    2. buffer + 2 – displacement for 3rd element in the array of
+    5 two dimensional arrays.
+
+    3. *(buffer + 2) – dereferencing, i.e. its type is now two dimensional array.
+    4. *(buffer + 2) + 1 – displacement to access 2nd element in the array of
+    7 one dimensional arrays.
+
+    5. *( *(buffer + 2) + 1) – dereferencing (accessing), now the type of
+    expression “*( *(buffer + 2) + 1)” is an array of integers.
+
+    6. *( *(buffer + 2) + 1) + 2 – displacement to get element at 3rd position
+    in the single dimension array of integers.
+
+    7. *( *( *(buffer + 2) + 1) + 2) – accessing the element at 3rd position
+    (the overall expression type is int now).
+     */
+
+    int buffer[2][3][4] = {
+        {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12}
+        },
+        {
+            {13, 14, 15, 16},
+            {17, 18, 19, 20},
+            {21, 22, 23, 24}
+        }
+    };
+
+    std::cout << "Element [1][2][3]: " << buffer[1][2][3] << std::endl;
+
+    std::cout << "Element [1][2][3] via pointer: " <<
+                 *(*(*(buffer + 1) + 2) + 3) << std::endl;
+
+    *(*(*(buffer + 1) + 2) + 3) = 60;
+    std::cout << "Element [1][2][3] via pointer after edit: " <<
+                 *(*(*(buffer + 1) + 2) + 3) << std::endl;
+}
+
 void Start() {
     InitArray();
     InitArrayString();
     PointerVsArray();
+    AccessElementIn2DArray();
 }
 
 }
