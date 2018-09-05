fb44a732aeba4b808ef97d2b166a68ccb8e47f17
iamantony <antony.cherepanov@gmail.com>
2018-09-05 18:29:07 +0300

    Add info about size of class with only one virtual method

--- src/classes/virtual_functions.hpp
+++ src/classes/virtual_functions.hpp
@@ -5,17 +5,15 @@
 #include <ostream>
 #include <memory>
 
-using namespace std;
+namespace Classes::VirtualFunctions {
 
-class Shape
-{
+class Shape {
 public:
     Shape() {}
     virtual ~Shape ( ) {}
 
-    virtual void draw(int x, int y)
-    {
-        cout << "Shape::draw(): " << x << ", " << y << endl;
+    virtual void draw(int x, int y) {
+        std::cout << "Shape::draw(): " << x << ", " << y << std::endl;
     }
 
 private:
@@ -23,65 +21,54 @@ private:
     Shape& operator=(const Shape&);
 };
 
-class Polygon : public Shape
-{
+class Polygon : public Shape {
 public:
-    virtual void draw ( int x, int y ) override
-    {
-        cout << "Polygon::draw(): " << x << ", " << y << endl;
+    virtual void draw ( int x, int y ) override {
+        std::cout << "Polygon::draw(): " << x << ", " << y << std::endl;
     }
 };
 
-class Evilgon : public Shape
-{
+class Evilgon : public Shape {
 public:
     // Here we define new function draw (shadowing draw() from Shape), because
     // of different types of function arguments
-    virtual void draw ( double x, double y )
-    {
-        cout << "Evilgon::draw(): " << x << ", " << y << endl;
+    virtual void draw ( double x, double y ) {
+        std::cout << "Evilgon::draw(): " << x << ", " << y << std::endl;
     }
 };
 
 // If we put "final" here, we would declare that class Triangle is final
 // derived class and we can't create another class (like BermudaTriangle),
 // which will inherit Triangle.
-class Triangle /*final*/ : public Polygon
-{
+class Triangle /*final*/ : public Polygon {
 public:
-
     // If we put "final" here (with or without override), we declare that
     // this overriding of function is final and we can't override it again in
     // child class of Triangle (BermudaTriangle in this example)
-    virtual void draw ( int x, int y ) /*final*/ override
-    {
-        cout << "Triangle::draw(): " << x << ", " << y << endl;
+    virtual void draw ( int x, int y ) /*final*/ override {
+        std::cout << "Triangle::draw(): " << x << ", " << y << std::endl;
     }
 };
 
-class BermudaTriangle : public Triangle
-{
+class BermudaTriangle : public Triangle {
 public:
-    virtual void draw ( int x, int y ) override
-    {
-        cout << "BermudaTriangle::draw(): " << x << ", " << y << endl;
+    virtual void draw ( int x, int y ) override {
+        std::cout << "BermudaTriangle::draw(): " << x << ", " << y << std::endl;
     }
 };
 
-class Base
-{
+class Base {
 public:
     virtual ~Base ( ) { }
 
     // Interface function
-    void draw(int x, int y)
-    {
-        cout << "Start of interface method Base::draw()" << endl;
+    void draw(int x, int y) {
+        std::cout << "Start of interface method Base::draw()" << std::endl;
 
         // Function (or functions) that do real work
         do_draw ( x, y );
 
-        cout << "Exit Base::draw()" << endl;
+        std::cout << "Exit Base::draw()" << std::endl;
     }
 
 private:
@@ -89,15 +76,13 @@ private:
     virtual void do_draw (int x, int y) = 0;
 };
 
-class Derived : public Base
-{
+class Derived : public Base {
 public:
     virtual ~Derived() override {}
 
 private:
-    virtual void do_draw (int, int) override
-    {
-        cout << "In Derived::do_draw() doing real work" << endl;
+    virtual void do_draw (int, int) override {
+        std::cout << "In Derived::do_draw() doing real work" << std::endl;
     }
 };
 
@@ -110,13 +95,13 @@ public:
 // Anyway we must provide implementation for ~Object. We could make it only
 // outside of class body.
 Object::~Object() {
-    cout << "Inside pure virtual destructor of Object" << endl;
+    std::cout << "Inside pure virtual destructor of Object" << std::endl;
 }
 
 class RealObject : public Object {
 public:
     virtual ~RealObject() override {
-        cout << "Inside virtual destructor of RealObject" << endl;
+        std::cout << "Inside virtual destructor of RealObject" << std::endl;
     }
 };
 
@@ -146,30 +131,64 @@ void Derived::Work() {
 
 }
 
+namespace ClassSize {
 
-void StartVF()
+class First {
+public:
+    int value;
+};
+
+class Second {
+public:
+    int value;
+
+    virtual ~Second() {}
+};
+
+}
+
+
+void Start()
 {
-    shared_ptr<Shape> shape = make_shared<Polygon>();
-    shape->draw(11, 12);
+    {
+        std::shared_ptr<Shape> shape = std::make_shared<Polygon>();
+        shape->draw(11, 12);
 
-    shape = make_shared<Evilgon>();
+        shape = std::make_shared<Evilgon>();
 
-    // Error - calling method Shape::draw(), but not Evilgon::draw();
-    // To block this error write virtual methods in derived classes like this:
-    // virtual void func(...) override {...}
-    // Special word override signals to compiler to carefully check this
-    // function
-    shape->draw (33, 44);
+        // Error - calling method Shape::draw(), but not Evilgon::draw();
+        // To block this error write virtual methods in derived classes like this:
+        // virtual void func(...) override {...}
+        // Special word override signals to compiler to carefully check this
+        // function
+        shape->draw (33, 44);
+    }
 
-    Derived d;
-    d.draw(4, 3);
+    {
+        Derived d;
+        d.draw(4, 3);
+    }
 
-    shared_ptr<Object> object = make_shared<RealObject> ( );
+    {
+        std::shared_ptr<Object> object = std::make_shared<RealObject>();
+    }
 
     {
         PVFIIBC::Derived derivedObject;
         derivedObject.Work();
     }
+
+    {
+        int number = 0;
+        ClassSize::First first;
+        ClassSize::Second second;
+
+        std::cout << "sizeof int: " << sizeof(number) << std::endl;
+        std::cout << "sizeof First: " << sizeof(first) << std::endl;
+        std::cout << "sizeof Second: " << sizeof(second) << std::endl;
+    }
+}
+
 }
 
 #endif /* VIRTUALFUNCTIONS_H_ */
