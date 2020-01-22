c7b85022dc884b7a6d241f465888117387d7e9c6
iamantony <antony.cherepanov@gmail.com>
2020-01-22 10:39:11 +0300

    Add not finished clone graph algorithm

---
+++ src/algorithms/data_structures/graph/clone_graph.hpp
@@ -0,0 +1,121 @@
+#ifndef CLONE_GRAPH_HPP
+#define CLONE_GRAPH_HPP
+
+/*
+https://leetcode.com/problems/clone-graph/
+Given a reference of a node in a connected undirected graph.
+Return a deep copy (clone) of the graph.
+Each node in the graph contains a val (int) and a list (List[Node]) of its neighbors.
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+
+
+Test case format:
+For simplicity sake, each node's value is the same as the node's index (1-indexed). For example,
+the first node with val = 1, the second node with val = 2, and so on. The graph is represented
+in the test case using an adjacency list.
+
+Adjacency list is a collection of unordered lists used to represent a finite graph. Each list
+describes the set of neighbors of a node in the graph.
+
+The given node will always be the first node with val = 1. You must return the copy of the given
+node as a reference to the cloned graph.
+
+Example 1:
+Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+Output: [[2,4],[1,3],[2,4],[1,3]]
+Explanation: There are 4 nodes in the graph.
+1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+
+Example 2:
+Input: adjList = [[]]
+Output: [[]]
+Explanation: Note that the input contains one empty list. The graph consists of only one node
+with val = 1 and it does not have any neighbors.
+
+Example 3:
+Input: adjList = []
+Output: []
+Explanation: This an empty graph, it does not have any nodes.
+
+Example 4:
+Input: adjList = [[2],[1]]
+Output: [[2],[1]]
+
+Constraints:
+- 1 <= Node.val <= 100
+- Node.val is unique for each node.
+- Number of Nodes will not exceed 100.
+- There is no repeated edges and no self-loops in the graph.
+- The Graph is connected and all nodes can be visited starting from the given node.
+*/
+
+#include <vector>
+#include <unordered_map>
+#include <stack>
+
+namespace Algo::DS::Graph {
+    class Node {
+    public:
+        int val;
+        std::vector<Node*> neighbors;
+
+        Node() {
+            val = 0;
+            neighbors = vector<Node*>();
+        }
+
+        Node(int _val) {
+            val = _val;
+            neighbors = vector<Node*>();
+        }
+
+        Node(int _val, vector<Node*> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    };
+
+    // TODO: use recursion
+    class CloneGraph {
+        static Node* deepClone(Node* node, ) {
+
+        }
+    public:
+        static Node* clone(Node* node) {
+            std::unordered_map<int, Node*> knownNodes;
+            std::stack<Node*> nodesToClone, clonedNodes;
+            nodesToClone.push(node);
+            while(!nodesToClone.empty()) {
+                auto* n = nodesToClone.top();
+                nodesToClone.pop();
+
+                if (knownNodes.count(n->val) > 0) {
+                    auto* createdNode = knownNodes[n->val];
+                    if (clonedNodes.empty()) {
+                        throw std::exception("Empty cloned nodes stack");
+                    }
+
+                    auto* prevNode = clonedNodes.top();
+                    prevNode->neighbors.push_back(createdNode);
+                }
+                else {
+                    auto* newNode = new Node(n->val);
+                    knownNodes.push_back(newNode->val, newNode);
+                    clonedNodes.push(newNode);
+                    for (auto* neighbor : n->neighbors) {
+                        // same code
+                    }
+                }
+            }
+        }
+    };
+}
+
+#endif //CLONE_GRAPH_HPP
