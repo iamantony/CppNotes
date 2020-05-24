ddc3c40c0553d2857a2f1eda53e17dc2d59f20cb
iamantony <antony.cherepanov@gmail.com>
2020-05-24 20:31:12 +0300

    Update primes.hpp
    - add namespace Algo::Math
    - add function GetNextPrime()

--- src/algorithms/math/primes.hpp
+++ src/algorithms/math/primes.hpp
@@ -5,126 +5,143 @@
 #include <cmath>
 #include <algorithm>
 
-template<typename T>
-std::vector<T> GetPrimesNaive(const T& limit)
-{
-    if (limit < 2)
+namespace Algo::Math {
+    template<typename T>
+    std::vector<T> GetPrimesNaive(const T& limit)
     {
-        return std::vector<T>();
-    }
-    else if (2 == limit)
-    {
-        return std::vector<T>(1, 2);
-    }
-
-    std::vector<T> primes;
-    for (T i = 2; i < limit; ++i)
-    {
-        bool isPrime = true;
-        for (T j = 2; j < i; ++j)
+        if (limit < 2)
         {
-            if (i % j == 0 && i != j)
+            return std::vector<T>();
+        }
+        else if (2 == limit)
+        {
+            return std::vector<T>(1, 2);
+        }
+
+        std::vector<T> primes;
+        for (T i = 2; i < limit; ++i)
+        {
+            bool isPrime = true;
+            for (T j = 2; j < i; ++j)
             {
-                isPrime = false;
-                break;
+                if (i % j == 0 && i != j)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                primes.push_back(i);
             }
         }
 
-        if (isPrime)
+        return primes;
+    }
+
+    template<typename T>
+    std::vector<T> GetPrimesImproved(const T& limit)
+    {
+        if (limit < 2)
         {
-            primes.push_back(i);
+            return std::vector<T>();
         }
-    }
-
-    return primes;
-}
-
-template<typename T>
-std::vector<T> GetPrimesImproved(const T& limit)
-{
-    if (limit < 2)
-    {
-        return std::vector<T>();
-    }
-    else if (2 == limit)
-    {
-        return std::vector<T>(1, 2);
-    }
-
-    std::vector<T> primes;
-    for (T i = 2; i < limit; ++i)
-    {
-        bool isPrime = true;
-        long double ldLimit = static_cast<long double>(i);
-        T newLimit = static_cast<T>(std::floor(std::sqrt(ldLimit)));
-        for (T j = 2; j <= newLimit; ++j)
+        else if (2 == limit)
         {
-            if (i % j == 0)
+            return std::vector<T>(1, 2);
+        }
+
+        std::vector<T> primes;
+        for (T i = 2; i < limit; ++i)
+        {
+            bool isPrime = true;
+            long double ldLimit = static_cast<long double>(i);
+            T newLimit = static_cast<T>(std::floor(std::sqrt(ldLimit)));
+            for (T j = 2; j <= newLimit; ++j)
             {
-                isPrime = false;
-                break;
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                primes.push_back(i);
             }
         }
 
-        if (isPrime)
+        return primes;
+    }
+
+    // Seive of Eratosthenes
+    template<typename T>
+    std::vector<T> GetPrimesSeive(const T& limit)
+    {
+        if (limit < 2)
         {
-            primes.push_back(i);
+            return std::vector<T>();
         }
-    }
-
-    return primes;
-}
-
-// Seive of Eratosthenes
-template<typename T>
-std::vector<T> GetPrimesSeive(const T& limit)
-{
-    if (limit < 2)
-    {
-        return std::vector<T>();
-    }
-    else if (2 == limit)
-    {
-        return std::vector<T>(1, 2);
-    }
-
-    size_t numOfValues = static_cast<size_t>(limit);
-    std::vector<bool> flags(numOfValues, true);
-    flags[0] = false;
-    flags[1] = false;
-
-    size_t prime = 2;
-    long double ldLimit = static_cast<long double>(limit);
-    size_t newLimit = static_cast<size_t>(std::floor(std::sqrt(ldLimit)));
-    while(prime <= newLimit)
-    {
-        // Cross out elements
-        for (size_t i = prime * prime; i < flags.size(); i += prime)
+        else if (2 == limit)
         {
-            flags[i] = false;
+            return std::vector<T>(1, 2);
         }
 
-        // Find first not cross out element - next prime number
-        size_t next = prime + 1;
-        while(next < flags.size() && false == flags[next])
+        size_t numOfValues = static_cast<size_t>(limit);
+        std::vector<bool> flags(numOfValues, true);
+        flags[0] = false;
+        flags[1] = false;
+
+        size_t prime = 2;
+        long double ldLimit = static_cast<long double>(limit);
+        size_t newLimit = static_cast<size_t>(std::floor(std::sqrt(ldLimit)));
+        while(prime <= newLimit)
         {
-            ++next;
+            // Cross out elements
+            for (size_t i = prime * prime; i < flags.size(); i += prime)
+            {
+                flags[i] = false;
+            }
+
+            // Find first not cross out element - next prime number
+            size_t next = prime + 1;
+            while(next < flags.size() && false == flags[next])
+            {
+                ++next;
+            }
+
+            prime = next;
         }
 
-        prime = next;
-    }
-
-    // Collect prime numbers
-    std::vector<T> primes;
-    for (size_t i = 0; i < flags.size(); ++i)
-    {
-        if (flags[i])
+        // Collect prime numbers
+        std::vector<T> primes;
+        for (size_t i = 0; i < flags.size(); ++i)
         {
-            primes.push_back(static_cast<T>(i));
+            if (flags[i])
+            {
+                primes.push_back(static_cast<T>(i));
+            }
         }
+
+        return primes;
     }
 
-    return primes;
+    template <typename T>
+    T GetNextPrime(const T& start_value) {
+        if (start_value < 2) { return {}; }
+
+        auto result = start_value;
+        auto prime_limit = start_value;
+        while (result <= start_value) {
+            prime_limit += start_value / 2;
+            const auto primes = GetPrimesSeive(prime_limit);
+            result = primes.back();
+        }
+
+        return result;
+    }
 }
 
 #endif /* PRIMES_HPP_ */
--- test/algorithms/math/test_primes.cpp
+++ test/algorithms/math/test_primes.cpp
@@ -7,15 +7,15 @@ BOOST_AUTO_TEST_SUITE(TestPrimesGeneration)
 
 BOOST_AUTO_TEST_CASE(naive_invalid)
 {
-    BOOST_CHECK(std::vector<int>() == GetPrimesNaive<int>(-10));
-    BOOST_CHECK(std::vector<int>() == GetPrimesNaive<int>(0));
-    BOOST_CHECK(std::vector<int>() == GetPrimesNaive<int>(1));
+    BOOST_CHECK(std::vector<int>() == Algo::Math::GetPrimesNaive<int>(-10));
+    BOOST_CHECK(std::vector<int>() == Algo::Math::GetPrimesNaive<int>(0));
+    BOOST_CHECK(std::vector<int>() == Algo::Math::GetPrimesNaive<int>(1));
 }
 
 BOOST_AUTO_TEST_CASE(naive_min)
 {
     std::vector<int> expected = {2};
-    BOOST_CHECK(expected == GetPrimesNaive<int>(2));
+    BOOST_CHECK(expected == Algo::Math::GetPrimesNaive<int>(2));
 }
 
 BOOST_AUTO_TEST_CASE(naive)
@@ -23,20 +23,20 @@ BOOST_AUTO_TEST_CASE(naive)
     std::vector<int> expected = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
             41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 
-    BOOST_CHECK(expected == GetPrimesNaive<int>(100));
+    BOOST_CHECK(expected == Algo::Math::GetPrimesNaive<int>(100));
 }
 
 BOOST_AUTO_TEST_CASE(improved_invalid)
 {
-    BOOST_CHECK(std::vector<int>() == GetPrimesImproved<int>(-10));
-    BOOST_CHECK(std::vector<int>() == GetPrimesImproved<int>(0));
-    BOOST_CHECK(std::vector<int>() == GetPrimesImproved<int>(1));
+    BOOST_CHECK(std::vector<int>() == Algo::Math::GetPrimesImproved<int>(-10));
+    BOOST_CHECK(std::vector<int>() == Algo::Math::GetPrimesImproved<int>(0));
+    BOOST_CHECK(std::vector<int>() == Algo::Math::GetPrimesImproved<int>(1));
 }
 
 BOOST_AUTO_TEST_CASE(improved_min)
 {
     std::vector<int> expected = {2};
-    BOOST_CHECK(expected == GetPrimesImproved<int>(2));
+    BOOST_CHECK(expected == Algo::Math::GetPrimesImproved<int>(2));
 }
 
 BOOST_AUTO_TEST_CASE(improved)
@@ -44,20 +44,20 @@ BOOST_AUTO_TEST_CASE(improved)
     std::vector<int> expected = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
             41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 
-    BOOST_CHECK(expected == GetPrimesImproved<int>(100));
+    BOOST_CHECK(expected == Algo::Math::GetPrimesImproved<int>(100));
 }
 
 BOOST_AUTO_TEST_CASE(seive_invalid)
 {
-    BOOST_CHECK(std::vector<int>() == GetPrimesSeive<int>(-10));
-    BOOST_CHECK(std::vector<int>() == GetPrimesSeive<int>(0));
-    BOOST_CHECK(std::vector<int>() == GetPrimesSeive<int>(1));
+    BOOST_CHECK(std::vector<int>() == Algo::Math::GetPrimesSeive<int>(-10));
+    BOOST_CHECK(std::vector<int>() == Algo::Math::GetPrimesSeive<int>(0));
+    BOOST_CHECK(std::vector<int>() == Algo::Math::GetPrimesSeive<int>(1));
 }
 
 BOOST_AUTO_TEST_CASE(seive_min)
 {
     std::vector<int> expected = {2};
-    BOOST_CHECK(expected == GetPrimesSeive<int>(2));
+    BOOST_CHECK(expected == Algo::Math::GetPrimesSeive<int>(2));
 }
 
 BOOST_AUTO_TEST_CASE(seive)
@@ -65,7 +65,7 @@ BOOST_AUTO_TEST_CASE(seive)
     std::vector<int> expected = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
             41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 
-    BOOST_CHECK(expected == GetPrimesSeive<int>(100));
+    BOOST_CHECK(expected == Algo::Math::GetPrimesSeive<int>(100));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
