238e7e9841b9287b833b68debaebe2fefcb26026
iamantony <antony.cherepanov@gmail.com>
2020-10-02 13:31:55 +0300

    Add algo - check if string is palindrom with one extra symbol

--- src/algorithms/data_structures/string/valid_palindrome.hpp
+++ src/algorithms/data_structures/string/valid_palindrome.hpp
@@ -1,20 +1,6 @@
 #ifndef VALID_PALINDROME_HPP
 #define VALID_PALINDROME_HPP
 
-// https://leetcode.com/problems/valid-palindrome/description/
-
-// Given a string, determine if it is a palindrome, considering only
-// alphanumeric characters and ignoring cases.
-
-// For example,
-// "A man, a plan, a canal: Panama" is a palindrome.
-// "race a car" is not a palindrome.
-
-// Note:
-// Have you consider that the string might be empty? This is a good question
-// to ask during an interview. For the purpose of this problem, we define empty
-// string as valid palindrome.
-
 #include <string>
 #include <algorithm>
 #include <locale>
@@ -22,6 +8,19 @@
 namespace Algo::DS::String {
     class ValidPalindrome {
     public:
+        /*
+        https://leetcode.com/problems/valid-palindrome/description/
+        Given a string, determine if it is a palindrome, considering only
+        alphanumeric characters and ignoring cases.
+
+        For example, "A man, a plan, a canal: Panama" is a palindrome.
+        "race a car" is not a palindrome.
+
+        Note:
+        Have you consider that the string might be empty? This is a good question
+        to ask during an interview. For the purpose of this problem, we define empty
+        string as valid palindrome.
+        */
         bool isPalindrome(std::string s) {
             if (s.size() < 2) {
                 return true;
@@ -50,6 +49,38 @@ namespace Algo::DS::String {
             }
 
             return true;
+        }
+
+        /*
+        https://leetcode.com/problems/valid-palindrome-ii/
+        Given a non-empty string s, you may delete at most one character. Judge whether you can
+        make it a palindrome.
+
+        Example 1:
+        Input: "aba"
+        Output: True
+
+        Example 2:
+        Input: "abca"
+        Output: True
+        Explanation: You could delete the character 'c'.
+
+        Note:
+        The string will only contain lowercase characters a-z. The maximum length of the string is
+        50000.
+        */
+        bool isPalindromeWithOneError(std::string s) {
+            if (s.size() < 2) { return true; }
+
+            size_t l = 0, r = s.size() - 1;
+            while (l < r  && s[l] == s[r]) {
+                ++l;
+                --r;
+            }
+
+            return l >= r ?
+                true :
+                isPalindrome({s.data() + l + 1, r-l}) || isPalindrome({s.data() + l, r-l});
         }
     };
 }
