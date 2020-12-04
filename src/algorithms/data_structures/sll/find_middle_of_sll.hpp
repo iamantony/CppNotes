a03110f4d622ce25bc4f2afb99e0485d0d910e02
iamantony <antony.cherepanov@gmail.com>
2020-12-04 12:44:52 +0300

    Update algo - find middle of sll

--- src/algorithms/data_structures/sll/find_middle_of_sll.hpp
+++ src/algorithms/data_structures/sll/find_middle_of_sll.hpp
@@ -1,31 +1,47 @@
 #ifndef FIND_MIDDLE_OF_SLL_HPP
 #define FIND_MIDDLE_OF_SLL_HPP
 
-// Find middle of the Singly Linked List
+/*
+https://leetcode.com/problems/middle-of-the-linked-list/
+Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+If there are two middle nodes, return the second middle node.
 
+Example 1:
+Input: [1,2,3,4,5]
+Output: Node 3 from this list (Serialization: [3,4,5])
+The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+Note that we returned a ListNode object ans, such that:
+ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+
+Example 2:
+Input: [1,2,3,4,5,6]
+Output: Node 4 from this list (Serialization: [4,5,6])
+Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+Note:
+The number of nodes in the given list will be between 1 and 100.
+*/
 #include "types/ds/singly_linked_list_nodes.hpp"
 
 namespace Algo::DS::SLL {
+    class FindMiddleElement {
+    public:
+        template<typename T>
+        static NodeSLL<T>* Find(NodeSLL<T>* head) {
+            if (nullptr == head) {
+                return nullptr;
+            }
 
-class FindMiddleElement {
-public:
-    template<typename T>
-    static NodeSLL<T>* Find(NodeSLL<T>* head) {
-        if (nullptr == head) {
-            return nullptr;
+            NodeSLL<T>* slowPtr = head;
+            NodeSLL<T>* fastPtr = head;
+            while(fastPtr != nullptr && fastPtr->next != nullptr) {
+                fastPtr = fastPtr->next->next; // move by two elements
+                slowPtr = slowPtr->next; // move by one element
+            }
+
+            return slowPtr;
         }
-
-        NodeSLL<T>* slowPtr = head;
-        NodeSLL<T>* fastPtr = head;
-        while(fastPtr != nullptr && fastPtr->next != nullptr) {
-            fastPtr = fastPtr->next->next; // move by two elements
-            slowPtr = slowPtr->next; // move by one element
-        }
-
-        return slowPtr;
-    }
-};
-
+    };
 }
 
 #endif // FIND_MIDDLE_OF_SLL_HPP
