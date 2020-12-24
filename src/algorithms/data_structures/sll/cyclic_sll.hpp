237be9874166f456e8bb1d437eb7a4b2942a58a2
iamantony <antony.cherepanov@gmail.com>
2020-12-24 18:38:55 +0300

    Add algo - get node that starts cycle in cyclic SLL

--- src/algorithms/data_structures/sll/cyclic_sll.hpp
+++ src/algorithms/data_structures/sll/cyclic_sll.hpp
@@ -1,49 +1,89 @@
 #ifndef CYCLIC_SLL_HPP
 #define CYCLIC_SLL_HPP
 
-// https://leetcode.com/problems/linked-list-cycle/description/
-
-// Given a linked list, determine if it has a cycle in it.
-
-// Follow up:
-// Can you solve it without using extra space?
-
-// Easy solution - use hash set. Iterate over SLL, putting new nodes into set,
-// and check if it already have same node. Time complexity: O(n); space
-// complexity: O(n)
-
-// More interesting solution - is to use two pointers: slow and fast.
-// If SLL is not cyclic, fast pointer will reach end of SLL.
-// If SLL is cyclic, fast pointer will catch up slow pointer.
-// Time complexity: O(n); space complexity: O(1)
-
 #include "types/ds/singly_linked_list_nodes.hpp"
 
 namespace Algo::DS::SLL {
+    template <typename T>
+    class CyclicSLL {
+    public:
+        /*
+        https://leetcode.com/problems/linked-list-cycle/
+        Given head, the head of a linked list, determine if the linked list has a cycle in it.
 
-class DetectCycle {
-public:
-    template<typename T>
-    static bool HasCycle(NodeSLL<T>* head) {
-        if (head == nullptr) {
+        There is a cycle in a linked list if there is some node in the list that can be reached
+        again by continuously following the next pointer. Internally, pos is used to denote the
+        index of the node that tail's next pointer is connected to. Note that pos is not passed
+        as a parameter.
+
+        Return true if there is a cycle in the linked list. Otherwise, return false.
+
+        Constraints:
+        * The number of the nodes in the list is in the range [0, 104].
+        * -10^5 <= Node.val <= 10^5
+        * pos is -1 or a valid index in the linked-list.
+
+        Follow up: Can you solve it using O(1) (i.e. constant) memory?
+        */
+        static bool HasCycle(NodeSLL<T>* head) {
+            if (head == nullptr) {
+                return false;
+            }
+
+            NodeSLL<T>* slow = head;
+            NodeSLL<T>* fast = head->next;
+            while(fast != nullptr && fast->next != nullptr) {
+                if (slow == fast) {
+                    return true;
+                }
+
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+
             return false;
         }
 
-        NodeSLL<T>* slow = head;
-        NodeSLL<T>* fast = head->next;
-        while(fast != nullptr && fast->next != nullptr) {
-            if (slow == fast) {
-                return true;
+        /*
+        https://leetcode.com/problems/linked-list-cycle-ii/
+        Given a linked list, return the node where the cycle begins. If there is no cycle,
+        return null.
+
+        There is a cycle in a linked list if there is some node in the list that can be reached
+        again by continuously following the next pointer. Internally, pos is used to denote the
+        index of the node that tail's next pointer is connected to. Note that pos is not passed
+        as a parameter.
+
+        Notice that you should not modify the linked list.
+
+        Constraints:
+        * The number of the nodes in the list is in the range [0, 104].
+        * -10^5 <= Node.val <= 10^5
+        * pos is -1 or a valid index in the linked-list.
+
+        Follow up: Can you solve it using O(1) (i.e. constant) memory?
+        */
+        static NodeSLL<T>* get_node_that_starts_cycle(NodeSLL<T>* head) {
+            if (head == nullptr || head->next == nullptr) { return nullptr; }
+
+            auto* slow  = head;
+            auto* fast  = head;
+            auto* entry = head;
+
+            while (fast->next && fast->next->next) {
+                slow = slow->next;
+                fast = fast->next->next;
+                if (slow == fast) {
+                    while(slow != entry) {
+                        slow  = slow->next;
+                        entry = entry->next;
+                    }
+                    return entry;
+                }
             }
-
-            slow = slow->next;
-            fast = fast->next->next;
+            return nullptr;
         }
-
-        return false;
-    }
-};
-
+    };
 }
 
 #endif // CYCLIC_SLL_HPP
--- test/algorithms/data_structures/sll/test_cyclic_sll.cpp
+++ test/algorithms/data_structures/sll/test_cyclic_sll.cpp
@@ -1,12 +1,12 @@
 #include <boost/test/unit_test.hpp>
 #include "algorithms/data_structures/sll/cyclic_sll.hpp"
 
-BOOST_AUTO_TEST_SUITE(CyclicSLL)
+BOOST_AUTO_TEST_SUITE(TestCyclicSLL)
 
 BOOST_AUTO_TEST_CASE(empty_list)
 {
     NodeSLL<int>* sll = nullptr;
-    BOOST_CHECK(false == Algo::DS::SLL::DetectCycle::HasCycle(sll));
+    BOOST_CHECK(false == Algo::DS::SLL::CyclicSLL<int>::HasCycle(sll));
     DeleteSLL(&sll);
 }
 
@@ -14,13 +14,13 @@ BOOST_AUTO_TEST_CASE(no_cycle)
 {
     {
         NodeSLL<int>* sll = CreateSLL<int>({10});
-        BOOST_CHECK(false == Algo::DS::SLL::DetectCycle::HasCycle(sll));
+        BOOST_CHECK(false == Algo::DS::SLL::CyclicSLL<int>::HasCycle(sll));
         DeleteSLL(&sll);
     }
 
     {
         NodeSLL<int>* sll = CreateSLL<int>({10, 9, 9, 10});
-        BOOST_CHECK(false == Algo::DS::SLL::DetectCycle::HasCycle(sll));
+        BOOST_CHECK(false == Algo::DS::SLL::CyclicSLL<int>::HasCycle(sll));
         DeleteSLL(&sll);
     }
 }
