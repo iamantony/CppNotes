b07386af34e5d6f8f495f3693ce266aa4e523df9
iamantony <antony.cherepanov@gmail.com>
2021-01-06 12:04:00 +0300

    Add new function to WordSearch class - find_words()

--- src/algorithms/data_structures/array/word_search.hpp
+++ src/algorithms/data_structures/array/word_search.hpp
@@ -28,6 +28,34 @@ Constraints:
 * board and word consists only of lowercase and uppercase English letters.
 */
 
+/*
+https://leetcode.com/problems/word-search-ii/
+Given an m x n board of characters and a list of strings words, return all words on the board.
+
+Each word must be constructed from letters of sequentially adjacent cells, where adjacent cells
+are horizontally or vertically neighboring. The same letter cell may not be used more than once
+in a word.
+
+Example 1:
+Input: board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]],
+words = ["oath","pea","eat","rain"]
+Output: ["eat","oath"]
+
+Example 2:
+Input: board = [["a","b"],["c","d"]], words = ["abcb"]
+Output: []
+
+Constraints:
+* m == board.length
+* n == board[i].length
+* 1 <= m, n <= 12
+* board[i][j] is a lowercase English letter.
+* 1 <= words.length <= 3 * 104
+* 1 <= words[i].length <= 10
+* words[i] consists of lowercase English letters.
+* All the strings of words are unique.
+*/
+
 #include <vector>
 #include <string>
 
@@ -84,6 +112,18 @@ namespace Algo::DS::Array {
             }
 
             return false;
+        }
+
+        std::vector<std::string> find_words(
+            std::vector<std::vector<char>>& board, std::vector<std::string>& words)
+        {
+            std::vector<std::string> res;
+            for (const auto& w : words) {
+                const auto tmp_board = board;
+                if (exist(tmp_board, w)) { res.push_back(w); }
+            }
+
+            return res;
         }
     };
 }
