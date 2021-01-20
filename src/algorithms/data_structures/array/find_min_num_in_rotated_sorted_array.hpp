df1c9ec2c5ada97740496924614211feb50d6810
iamantony <antony.cherepanov@gmail.com>
2021-01-20 10:19:25 +0300

    Add algo - find min num in rotated sorted array with duplicates

--- src/algorithms/data_structures/array/find_min_num_in_rotated_sorted_array.hpp
+++ src/algorithms/data_structures/array/find_min_num_in_rotated_sorted_array.hpp
@@ -36,13 +36,34 @@ Constraints:
 * nums is sorted and rotated between 1 and n times.
 */
 
+/*
+https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+
+Find the minimum element. The array may contain duplicates.
+
+Example 1:
+Input: [1,3,5]
+Output: 1
+
+Example 2:
+Input: [2,2,2,0,1]
+Output: 0
+
+Note:
+* This is a follow up problem to Find Minimum in Rotated Sorted Array.
+* Would allow duplicates affect the run-time complexity? How and why?
+*/
+
 #include <vector>
 
 namespace Algo::DS::Array {
     template <typename T>
     class FindMinValue {
     public:
-        T find_in_rotated_sorted_array(const vector<T>& nums) {
+        T find_in_rotated_sorted_array(const std::vector<T>& nums) {
             if (nums.empty()) { return -1; }
             if (nums.size() == 1) { return nums.front(); }
 
@@ -70,6 +91,23 @@ namespace Algo::DS::Array {
             }
 
             return nums[pos];
+        }
+
+        T find_in_rotated_sorted_array_with_duplicates(const std::vector<T>& nums) {
+            if (nums.empty()) { return -1; }
+            if (nums.size() == 1) { return nums.front(); }
+
+            size_t left = 0;
+            size_t right = nums.size() - 1;
+            while (left < right)
+            {
+                auto mid = left + (right - left) / 2;
+                if (nums[mid] > nums[right]) { left = mid + 1; }
+                else if (nums[mid] < nums[right]) { right = mid; }
+                else { --right; }
+            }
+
+            return nums[left];
         }
     };
 }
